3d285289622db4a84ec82060d34248552f6197c0
Jorel Ali <jorel.k.ali@gmail.com>
2018-11-12 22:24:31 +0000

    Adds 1.6 to repo

---
+++ 1.13CommandAPI/mvn-repo-generation.sh
@@ -0,0 +1,1 @@
+mvn install:install-file -DgroupId=io.github.jorelali -DartifactId=commandapi -Dversion=1.6 -Dfile=./CommandAPI.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
