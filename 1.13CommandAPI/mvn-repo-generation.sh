ded09d3f0dc0e49fd3c8b0755cc6e6ddf792004f
Jorel Ali <jorel.k.ali@gmail.com>
2018-12-01 15:19:29 +0000

    Adds 1.7 to mvn repo

--- 1.13CommandAPI/mvn-repo-generation.sh
+++ 1.13CommandAPI/mvn-repo-generation.sh
@@ -1,1 +1,1 @@
-mvn install:install-file -DgroupId=io.github.jorelali -DartifactId=commandapi -Dversion=1.6 -Dfile=./CommandAPI.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
+mvn install:install-file -DgroupId=io.github.jorelali -DartifactId=commandapi -Dversion=1.7 -Dfile=./CommandAPI.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
