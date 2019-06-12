140676c4c535ac1f35a4587dbfefde8421c935c4
Jorel Ali <jorel.k.ali@gmail.com>
2019-06-12 17:43:35 +0100

    Adds download spigot script

---
+++ downloadSpigot.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+if [ -f ./BuildTools.jar ]; then
+	if [ ! -f ./spigot-1.13.2.jar ]; then
+		echo "Building Spigot 1.13.2"
+		java -jar BuildTools.jar --rev 1.13.2
+	fi
+
+	if [ ! -f ./spigot-1.14.jar ]; then
+		echo "Building Spigot 1.14"
+		java -jar BuildTools.jar --rev 1.14
+	fi
+
+	if [ ! -f ./spigot-1.14.1.jar ]; then
+		echo "Building Spigot 1.14.1"
+		java -jar BuildTools.jar --rev 1.14.1
+	fi
+fi
+echo "Copying files to ./spigotlibs"
+mkdir spigotlibs
+cp ./spigot-1.13.2.jar spigotlibs
+cp ./spigot-1.14.jar spigotlibs
+cp ./spigot-1.14.1.jar spigotlibs
+echo "Done!"
