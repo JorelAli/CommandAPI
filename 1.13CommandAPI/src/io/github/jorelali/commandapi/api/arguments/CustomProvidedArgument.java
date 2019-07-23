a41e686c2538b14e59de26192933df903259fd3f
portlek <hsndmrts_@outlook.com>
2019-07-23 14:33:22 +0300

    unnecessary things

--- 1.13CommandAPI/src/io/github/jorelali/commandapi/api/arguments/CustomProvidedArgument.java
+++ 1.13CommandAPI/src/io/github/jorelali/commandapi/api/arguments/CustomProvidedArgument.java
@@ -2,9 +2,9 @@ package io.github.jorelali.commandapi.api.arguments;
 
 public interface CustomProvidedArgument {
 			
-	public enum SuggestionProviders {
-		FUNCTION, RECIPES, SOUNDS, ADVANCEMENTS, LOOT_TABLES;
+	enum SuggestionProviders {
+		FUNCTION, RECIPES, SOUNDS, ADVANCEMENTS, LOOT_TABLES
 	}
 	
-	public SuggestionProviders getSuggestionProvider();
+	SuggestionProviders getSuggestionProvider();
 }
