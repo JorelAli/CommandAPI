df33bad6d522fb4a7a605c543e61b3ccc2646168
Jorel Ali <contact@jorel.dev>
2020-06-07 17:19:22 +0100

    work on argument refactor

---
+++ 1.13CommandAPI/commandapi-core/src/main/java/io/github/jorelali/commandapi/api/arguments/ZArg2.java
@@ -0,0 +1,18 @@
+package io.github.jorelali.commandapi.api.arguments;
+
+@SuppressWarnings("unchecked")
+public class ZArg2 extends ZArg {
+
+	@Override
+	public <V> Class<V> getPrimitiveType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CommandAPIArgumentType getArgumentType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
