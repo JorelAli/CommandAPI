a550b0eb8d39630920def54b2383a00a18b752e3
Jorel Ali <contact@jorel.dev>
2020-06-25 11:24:02 +0100

    Update maven repo for 3.0

--- 1.13CommandAPI/mvn-repo-generation.sh
+++ 1.13CommandAPI/mvn-repo-generation.sh
@@ -2,4 +2,4 @@
 echo "Enter the version (For example, 1.7): "
 read version
 perl -p -i -e "s|<version>dev</version>|<version>$version</version>|g" ./pom.xml
-mvn install:install-file -DgroupId=io.github.jorelali -DartifactId=commandapi -Dversion=$version -Dfile=./CommandAPI.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true -DpomFile=./pom.xml
+mvn install:install-file -DgroupId=dev.jorel -DartifactId=commandapi -Dversion=$version -Dfile=./CommandAPI.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true -DpomFile=./pom.xml
