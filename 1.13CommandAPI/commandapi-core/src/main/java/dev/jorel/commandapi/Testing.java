46a592005ecaae44b2a85bce3b8ceb09db520f79
Jorel Ali <contact@jorel.dev>
2020-06-25 23:20:51 +0100

    Remove testing code; preparing for 3.0 release now.

--- 1.13CommandAPI/commandapi-core/src/main/java/dev/jorel/commandapi/Testing.java
+++
@@ -1,9 +0,0 @@
-package dev.jorel.commandapi;
-
-public class Testing {
-	public static void registerTestCommands() {
-		new CommandAPICommand("a:b").executes((sender, args) -> {
-			System.out.println("hi");
-		}).register();
-	}
-}
