ad6580707b7122bb1e607e30b48c47df80fc31c8
JorelAli <jorel.k.ali@gmail.com>
2020-06-29 10:41:45 +0100

    Significantly improves downloadSpigot.sh with maven

--- downloadSpigot.sh
+++ downloadSpigot.sh
@@ -1,63 +1,26 @@
 #!/bin/sh
 if [ -f ./BuildTools.jar ]; then
-	if [ ! -f ./spigot-1.13.jar ]; then
-		echo "Building Spigot 1.13"
-		java -jar BuildTools.jar --rev 1.13
-	fi
-	
-	if [ ! -f ./spigot-1.13.1.jar ]; then
-		echo "Building Spigot 1.13.1"
-		java -jar BuildTools.jar --rev 1.13.1
-	fi
-	
-	if [ ! -f ./spigot-1.13.2.jar ]; then
-		echo "Building Spigot 1.13.2"
-		java -jar BuildTools.jar --rev 1.13.2
-	fi
 
-	if [ ! -f ./spigot-1.14.jar ]; then
-		echo "Building Spigot 1.14"
-		java -jar BuildTools.jar --rev 1.14
-	fi
-	
-	if [ ! -f ./spigot-1.14.1.jar ]; then
-		echo "Building Spigot 1.14.1"
-		java -jar BuildTools.jar --rev 1.14.1
-	fi
-	
-	if [ ! -f ./spigot-1.14.2.jar ]; then
-		echo "Building Spigot 1.14.2"
-		java -jar BuildTools.jar --rev 1.14.2
-	fi
-	
-	if [ ! -f ./spigot-1.14.3.jar ]; then
-		echo "Building Spigot 1.14.3"
-		java -jar BuildTools.jar --rev 1.14.3
-	fi
-	
-	if [ ! -f ./spigot-1.14.4.jar ]; then
-		echo "Building Spigot 1.14.4"
-		java -jar BuildTools.jar --rev 1.14.4
-	fi
+	echo "BuildTools.jar found. Checking for available versions of spigot:"
 
-	if [ ! -f ./spigot-1.15.2.jar ]; then
-		echo "Building Spigot 1.15.2"
-		java -jar BuildTools.jar --rev 1.15.2
-	fi
+	mkdir buildspigot
+	cp BuildTools.jar buildspigot
+	cd buildspigot
 
-	if [ ! -f ./spigot-1.16.1.jar ]; then
-		echo "Building Spigot 1.16.1"
-		java -jar BuildTools.jar --rev 1.16.1
-	fi
+	versions=(1.13 1.13.1 1.13.2 1.14 1.14.3 1.14.4 1.15 1.16.1)
+
+	for version in ${versions[@]}; do
+		if mvn dependency:get --batch-mode -q -Dartifact=org.spigotmc:spigot:${version}-R0.1-SNAPSHOT; then
+			echo "Found spigot version $version"
+		else
+			echo "Building Spigot $version"
+			java -jar BuildTools.jar --rev $version
+		fi
+	done
+
+	cd ..
+	rm -rf buildspigot
+else
+	echo "Couldn't find the BuildTools.jar!"
 fi
-echo "Copying files to ./spigotlibs"
-mkdir spigotlibs
-cp ./spigot-1.13.jar spigotlibs
-cp ./spigot-1.13.1.jar spigotlibs
-cp ./spigot-1.13.2.jar spigotlibs
-cp ./spigot-1.14.2.jar spigotlibs
-cp ./spigot-1.14.3.jar spigotlibs
-cp ./spigot-1.14.4.jar spigotlibs
-cp ./spigot-1.15.2.jar spigotlibs
-cp ./spigot-1.16.1.jar spigotlibs
 echo "Done!"
