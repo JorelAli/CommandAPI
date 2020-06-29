0e2e01b123a5779a196ffdefc86c3f6da9757877
ryuuta0217 <ryuuta.iwakura@gmail.com>
2020-06-29 13:31:37 +0900

    A very tiny changes
    don't let git commit files that don't want... (.gitignore)
    
    "org.spigotmc:spigot:1.15-R0.1-SNAPSHOT" is when you run "downloadSpigot.bat", it doesn't add it to the local repository. The build fails, so I changed to a more reliable method. (1.13CommandAPI/commandapi-${mc-ver}/pom.xml)
    
    I changed the batch file a bit because I didn't want the garbage left in the root folder of the project after I built Spigot. (downloadSpigot.bat)

--- downloadSpigot.bat
+++ downloadSpigot.bat
@@ -1,24 +1,45 @@
 @echo off
-if exist BuildTools.jar (
-	mkdir spigotlibs
-	if not exist spigot-1.13.jar echo Building Spigot 1.13 && java -jar BuildTools.jar --rev 1.13
-	if not exist spigot-1.13.1.jar echo Building Spigot 1.13.1 && java -jar BuildTools.jar --rev 1.13.1
-	if not exist spigot-1.13.2.jar echo Building Spigot 1.13.2 && java -jar BuildTools.jar --rev 1.13.2
-	if not exist spigot-1.14.2.jar echo Building Spigot 1.14.2 && java -jar BuildTools.jar --rev 1.14.2
-	if not exist spigot-1.14.3.jar echo Building Spigot 1.14.3 && java -jar BuildTools.jar --rev 1.14.3
-	if not exist spigot-1.14.4.jar echo Building Spigot 1.14.4 && java -jar BuildTools.jar --rev 1.14.4
-	if not exist spigot-1.15.2.jar echo Building Spigot 1.15.2 && java -jar BuildTools.jar --rev 1.15.2
-	if not exist spigot-1.16.1.jar echo Building Spigot 1.16.1 && java -jar BuildTools.jar --rev 1.16.1
+:start
+if not exist build-works (
+    mkdir build-works
+    if %ERRORLEVEL% == 0 (
+        echo Created folder: build-works
+    )
+)
+
+if not exist libs (
+    mkdir libs
+    if %ERRORLEVEL% == 0 (
+        echo Created folder: libs
+    )
+)
+
+if exist build-works\BuildTools.jar (
+	cd build-works
+	if not exist spigot-1.13.jar title Building Spigot 1.13 && java -jar BuildTools.jar --rev 1.13
+	if not exist spigot-1.13.1.jar title Building Spigot 1.13.1 && java -jar BuildTools.jar --rev 1.13.1
+	if not exist spigot-1.13.2.jar title Building Spigot 1.13.2 && java -jar BuildTools.jar --rev 1.13.2
+	if not exist spigot-1.14.2.jar title Building Spigot 1.14.2 && java -jar BuildTools.jar --rev 1.14.2
+	if not exist spigot-1.14.3.jar title Building Spigot 1.14.3 && java -jar BuildTools.jar --rev 1.14.3
+	if not exist spigot-1.14.4.jar title Building Spigot 1.14.4 && java -jar BuildTools.jar --rev 1.14.4
+	if not exist spigot-1.15.2.jar title Building Spigot 1.15.2 && java -jar BuildTools.jar --rev 1.15.2
+	if not exist spigot-1.16.1.jar title Building Spigot 1.16.1 && java -jar BuildTools.jar --rev 1.16.1
+	cd ..
 	
-	copy spigot-1.13.jar spigotlibs
-	copy spigot-1.13.1.jar spigotlibs
-	copy spigot-1.13.2.jar spigotlibs
-	copy spigot-1.14.2.jar spigotlibs
-	copy spigot-1.14.3.jar spigotlibs
-	copy spigot-1.14.4.jar spigotlibs
-	copy spigot-1.15.2.jar spigotlibs
-	copy spigot-1.16.1.jar spigotlibs
+	title Copying files
+	copy build-works\spigot-1.13.jar libs
+	copy build-works\spigot-1.13.1.jar libs
+	copy build-works\spigot-1.13.2.jar libs
+	copy build-works\spigot-1.14.2.jar libs
+	copy build-works\spigot-1.14.3.jar libs
+	copy build-works\spigot-1.14.4.jar libs
+	copy build-works\spigot-1.15.2.jar libs
+	copy build-works\spigot-1.16.1.jar libs
 	
+	title Done!
 	echo Done!
 	pause
-) else (echo BuildTool.jar not found! && pause)
+) else (
+	bitsadmin /transfer DownloadBT https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar %CD%\build-works\BuildTools.jar
+	goto start
+)
