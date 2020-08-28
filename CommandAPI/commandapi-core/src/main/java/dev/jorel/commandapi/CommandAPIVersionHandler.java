5148de34bba60fe9f07e0d338b1472fcca064bae
Jorel Ali <contact@jorel.dev>
2020-08-28 12:19:25 +0100

    Throw RE when trying to use the wrong version handler

--- CommandAPI/commandapi-core/src/main/java/dev/jorel/commandapi/CommandAPIVersionHandler.java
+++ CommandAPI/commandapi-core/src/main/java/dev/jorel/commandapi/CommandAPIVersionHandler.java
@@ -1,8 +1,5 @@
 package dev.jorel.commandapi;
 
-import java.lang.reflect.InvocationTargetException;
-
-import dev.jorel.commandapi.exceptions.UnsupportedVersionException;
 import dev.jorel.commandapi.nms.NMS;
 
 /**
@@ -14,52 +11,7 @@ import dev.jorel.commandapi.nms.NMS;
 public interface CommandAPIVersionHandler {
 	
 	public static NMS getNMS(String version) {
-
-		NMS nms = null;
-		
-		// Handle versioning
-		try {
-			switch (version) {
-			case "1.13":
-				nms = (NMS) Class.forName("dev.jorel.commandapi.nms.NMS_1_13").getDeclaredConstructor()
-						.newInstance();
-				break;
-			case "1.13.1":
-				nms = (NMS) Class.forName("dev.jorel.commandapi.nms.NMS_1_13_1").getDeclaredConstructor()
-						.newInstance();
-				break;
-			case "1.13.2":
-				nms = (NMS) Class.forName("dev.jorel.commandapi.nms.NMS_1_13_2").getDeclaredConstructor()
-						.newInstance();
-				break;
-			case "1.14":
-			case "1.14.1":
-			case "1.14.2":
-				nms = (NMS) Class.forName("dev.jorel.commandapi.nms.NMS_1_14").getDeclaredConstructor()
-						.newInstance();
-				break;
-			case "1.14.3":
-				nms = (NMS) Class.forName("dev.jorel.commandapi.nms.NMS_1_14_3").getDeclaredConstructor()
-						.newInstance();
-				break;
-			case "1.14.4":
-				nms = (NMS) Class.forName("dev.jorel.commandapi.nms.NMS_1_14_4").getDeclaredConstructor()
-						.newInstance();
-				break;
-			case "1.15":
-			case "1.15.1":
-			case "1.15.2":
-				nms = (NMS) Class.forName("dev.jorel.commandapi.nms.NMS_1_15").getDeclaredConstructor()
-						.newInstance();
-				break;
-			default:
-				throw new UnsupportedVersionException("This version of Minecraft is unsupported: " + version);
-			}
-		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
-				| NoSuchMethodException | SecurityException | ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		return nms;
+		throw new RuntimeException("You have the wrong copy of the CommandAPI! Make sure to use the one from https://github.com/JorelAli/CommandAPI/releases");
 	}
 	
 }
