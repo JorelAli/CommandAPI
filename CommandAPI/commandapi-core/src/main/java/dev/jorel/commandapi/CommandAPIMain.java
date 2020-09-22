705f4aaaa7b744948ba244b2ca765068c635734c
Jorel Ali <contact@jorel.dev>
2020-09-22 18:19:46 +0100

    Fix nonsense with permission logging

--- CommandAPI/commandapi-core/src/main/java/dev/jorel/commandapi/CommandAPIMain.java
+++ CommandAPI/commandapi-core/src/main/java/dev/jorel/commandapi/CommandAPIMain.java
@@ -12,5 +12,11 @@ public class CommandAPIMain extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		CommandAPI.onEnable(this);
+		
+		new CommandAPICommand("mycommand")
+		.withPermission(CommandPermission.OP)
+		.executes((s, a) -> {
+			System.out.println("hi");
+		}).register();
 	}
 }
