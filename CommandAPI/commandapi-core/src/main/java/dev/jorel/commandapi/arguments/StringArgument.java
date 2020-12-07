4b92ef9800848f28e22592b8d49310b4859426dc
Jorel Ali <contact@jorel.dev>
2020-12-07 12:42:42 +0000

    Makes string arguments overrideable safely

--- CommandAPI/commandapi-core/src/main/java/dev/jorel/commandapi/arguments/StringArgument.java
+++ CommandAPI/commandapi-core/src/main/java/dev/jorel/commandapi/arguments/StringArgument.java
@@ -5,14 +5,14 @@ import com.mojang.brigadier.arguments.StringArgumentType;
 /**
  * An argument that represents a simple String
  */
-public class StringArgument extends Argument {
+public class StringArgument extends SafeOverrideableArgument<String> {
 
 	/**
 	 * A string argument for one word
 	 * @param nodeName the name of the node for this argument
 	 */
 	public StringArgument(String nodeName) {
-		super(nodeName, StringArgumentType.word());
+		super(nodeName, StringArgumentType.word(), s -> s);
 	}
 
 	@Override
--- CommandAPI/commandapi-plugin/src/main/java/dev/jorel/commandapi/CommandAPIMain.java
+++ CommandAPI/commandapi-plugin/src/main/java/dev/jorel/commandapi/CommandAPIMain.java
@@ -3,15 +3,9 @@ package dev.jorel.commandapi;
 import java.io.File;
 import java.util.Map.Entry;
 
-import org.bukkit.Bukkit;
-import org.bukkit.World;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import dev.jorel.commandapi.arguments.CustomArgument;
-import dev.jorel.commandapi.arguments.IntegerArgument;
-import dev.jorel.commandapi.arguments.SafeOverrideableArgument;
 
 public class CommandAPIMain extends JavaPlugin implements Listener {
 	
@@ -43,18 +37,6 @@ public class CommandAPIMain extends JavaPlugin implements Listener {
 	public void onEnable() {
 		CommandAPI.onEnable(this);
 		getServer().getPluginManager().registerEvents(this, this);
-		
-SafeOverrideableArgument<World> customArg = new CustomArgument<World>("world", Bukkit::getWorld);
-customArg.overrideSuggestionsT(/*blah*/);
-
-
-new CommandAPICommand("tpworld")
-	.withOptionalArgument(customArg, Bukkit.getWorld("world"))
-	.withOptionalArgument(new IntegerArgument("arg").overrideSuggestions(""), 2)
-	.executesPlayer((player, args) -> {
-		// ... code
-	})
-	.register();
 
 	}
 }
