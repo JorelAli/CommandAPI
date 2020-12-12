dc81499bff0d04cf88cccb123744136087c27b84
Jorel Ali <contact@jorel.dev>
2020-12-12 11:55:00 +0000

    Change version numbers to 5.4

--- update.sh
+++ update.sh
@@ -3,22 +3,6 @@ echo "Old version: "
 read oldVer
 echo "New version: "
 read newVer
-sed -i "s/<version>$oldVer<\/version>/<version>$newVer<\/version>/" CommandAPI/commandapi-1.13/pom.xml
-sed -i "s/<version>$oldVer<\/version>/<version>$newVer<\/version>/" CommandAPI/commandapi-1.13.1/pom.xml
-sed -i "s/<version>$oldVer<\/version>/<version>$newVer<\/version>/" CommandAPI/commandapi-1.13.2/pom.xml
-sed -i "s/<version>$oldVer<\/version>/<version>$newVer<\/version>/" CommandAPI/commandapi-1.14/pom.xml
-sed -i "s/<version>$oldVer<\/version>/<version>$newVer<\/version>/" CommandAPI/commandapi-1.14.3/pom.xml
-sed -i "s/<version>$oldVer<\/version>/<version>$newVer<\/version>/" CommandAPI/commandapi-1.14.4/pom.xml
-sed -i "s/<version>$oldVer<\/version>/<version>$newVer<\/version>/" CommandAPI/commandapi-1.15/pom.xml
-sed -i "s/<version>$oldVer<\/version>/<version>$newVer<\/version>/" CommandAPI/commandapi-1.16.1/pom.xml
-sed -i "s/<version>$oldVer<\/version>/<version>$newVer<\/version>/" CommandAPI/commandapi-1.16.2/pom.xml
-sed -i "s/<version>$oldVer<\/version>/<version>$newVer<\/version>/" CommandAPI/commandapi-1.16.4/pom.xml
-
-sed -i "s/<version>$oldVer<\/version>/<version>$newVer<\/version>/" CommandAPI/commandapi-core/pom.xml
-sed -i "s/<version>$oldVer<\/version>/<version>$newVer<\/version>/" CommandAPI/commandapi-plugin/pom.xml
-sed -i "s/<version>$oldVer<\/version>/<version>$newVer<\/version>/" CommandAPI/commandapi-shade/pom.xml
-sed -i "s/<version>$oldVer<\/version>/<version>$newVer<\/version>/" CommandAPI/commandapi-vh/pom.xml
-sed -i "s/<version>$oldVer<\/version>/<version>$newVer<\/version>/" CommandAPI/pom.xml
 
 sed -i "s/<version>$oldVer<\/version>/<version>$newVer<\/version>/" docssrc/src/shading.md
 sed -i "s/<version>$oldVer<\/version>/<version>$newVer<\/version>/" docssrc/src/quickstart.md
@@ -30,3 +14,7 @@ sed -i "s/version: $oldVer/version: $newVer/" CommandAPI/commandapi-plugin/src/m
 
 sed -i "s/PROJECT_NUMBER         = $oldVer/PROJECT_NUMBER         = $newVer/" Doxyfile
 sed -i "s/$oldVer/$newVer/" docssrc/book.toml
+
+cd CommandAPI
+mvn versions:set -DnewVersion=$newVer
+mvn versions:commit
