203661e0dc01bfc4c4539e3a1225f3fd4fcbf3aa
Jorel Ali <contact@jorel.dev>
2021-06-15 21:07:35 +0100

    Update jitpacksetup.sh

--- jitpacksetup.sh
+++ jitpacksetup.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
-echo "Installing Spigot libraries..."
-mvn org.apache.maven.plugins:maven-dependency-plugin:2.1:get -DrepoUrl=https://repo.codemc.io/repository/nms/ -Dartifact=org.spigotmc:spigot:1.13-R0.1-SNAPSHOT
-mvn org.apache.maven.plugins:maven-dependency-plugin:2.1:get -DrepoUrl=https://repo.codemc.io/repository/nms/ -Dartifact=org.spigotmc:spigot:1.13.1-R0.1-SNAPSHOT
-mvn org.apache.maven.plugins:maven-dependency-plugin:2.1:get -DrepoUrl=https://repo.codemc.io/repository/nms/ -Dartifact=org.spigotmc:spigot:1.13.2-R0.1-SNAPSHOT
-mvn org.apache.maven.plugins:maven-dependency-plugin:2.1:get -DrepoUrl=https://repo.codemc.io/repository/nms/ -Dartifact=org.spigotmc:spigot:1.14-R0.1-SNAPSHOT
-mvn org.apache.maven.plugins:maven-dependency-plugin:2.1:get -DrepoUrl=https://repo.codemc.io/repository/nms/ -Dartifact=org.spigotmc:spigot:1.14.3-R0.1-SNAPSHOT
-mvn org.apache.maven.plugins:maven-dependency-plugin:2.1:get -DrepoUrl=https://repo.codemc.io/repository/nms/ -Dartifact=org.spigotmc:spigot:1.14.4-R0.1-SNAPSHOT
-mvn org.apache.maven.plugins:maven-dependency-plugin:2.1:get -DrepoUrl=https://repo.codemc.io/repository/nms/ -Dartifact=org.spigotmc:spigot:1.15-R0.1-SNAPSHOT
-mvn org.apache.maven.plugins:maven-dependency-plugin:2.1:get -DrepoUrl=https://repo.codemc.io/repository/nms/ -Dartifact=org.spigotmc:spigot:1.16.1-R0.1-SNAPSHOT
-mvn org.apache.maven.plugins:maven-dependency-plugin:2.1:get -DrepoUrl=https://repo.codemc.io/repository/nms/ -Dartifact=org.spigotmc:spigot:1.16.2-R0.1-SNAPSHOT
-mvn org.apache.maven.plugins:maven-dependency-plugin:2.1:get -DrepoUrl=https://repo.codemc.io/repository/nms/ -Dartifact=org.spigotmc:spigot:1.16.4-R0.1-SNAPSHOT
-mvn org.apache.maven.plugins:maven-dependency-plugin:2.1:get -DrepoUrl=https://repo.codemc.io/repository/nms/ -Dartifact=org.spigotmc:spigot:1.16.5-R0.1-SNAPSHOT
-echo "Done!"
+
+echo "Old Java version:"
+java -version
+
+echo "Installing Java 16"
+wget https://github.com/sormuras/bach/raw/master/install-jdk.sh
+source install-jdk.sh --feature 16
+java -version
