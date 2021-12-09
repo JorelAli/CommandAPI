eb76ece57c549614799547e59d46a88b3876dde7
469512345 <e69512345@gmail.com>
2021-12-09 14:06:30 +0000

    Building a command using a Tree-like syntax (#253)
    * 6.0.5 -> 6.1.0. Adds some (incomplete) boilerplate code for help topics
    
    * Adds main backend implementation for help topics
    
    * Populate list of help topics with help topics
    
    * Deal with generic CustomCommandExecutor type warnings
    
    * Tweak short description text. Adds TODOs + forRemoval deprecation
    
    * Adds converted help topics; Fix issue with help perms + truncation
    
    * Fix aliases in help topics
    
    * Fixed short descriptions and plugin conflicted commands
    
    * Finish up help topic usages. Document CommandAPICommand functions
    
    * Adds annotation support for help
    
    * Remove a varhandle invocation
    
    functionPermissionLevel is a constant, which we can access via a very handy method in the MinecraftServer, so we don't have to jump through hoops to get it
    
    * The fix for Bukkit's /reload command
    
    * Clean up NMS 1.17 implementation + add SafeReflect parameters
    
    * Adds reload implementation for 1.16.5
    
    * Suggestions are now handled asynchronously using CompletableFuture, increasing their versatility
    
    * Fix bug where included suggestions would be ignored
    
    * Interface for providing suggestions, which removes need for lots of overridden suggestions methods.
    
    * Added CommandSyntaxException to ArgumentSuggestions for giving native parsing exceptions
    
    * Adds support for a tree structure when registering a command.
    
    * Merge
    
    * Add / improve documentation
    
    Co-authored-by: Jorel Ali <github@jorel.dev>

---
+++ CommandAPI/commandapi-core/src/main/java/dev/jorel/commandapi/ArgumentTree.java
@@ -0,0 +1,47 @@
+package dev.jorel.commandapi;
+
+import dev.jorel.commandapi.arguments.Argument;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * This is a base class for arguments, allowing them to behave as tree nodes in a {@link CommandTree}
+ */
+public class ArgumentTree extends Executable<ArgumentTree> {
+
+	final List<Execution> executions = new LinkedList<>();
+	final Argument argument;
+
+	protected ArgumentTree() {
+		if(!(this instanceof Argument argument)) {
+			throw new IllegalArgumentException("Implicit inherited constructor must be from Argument");
+		}
+		this.argument = argument;
+	}
+
+	public ArgumentTree(final Argument argument) {
+		this.argument = argument;
+		//Copy the executor in case any executions were defined on the argument
+		this.executor = argument.executor;
+	}
+
+	/**
+	 * Create a child branch on this node
+	 * @param tree The child branch
+	 * @return this tree node
+	 */
+	public ArgumentTree then(final ArgumentTree tree) {
+		//Add all of the executions
+		for(Execution execution : tree.executions) {
+			//Prepend this argument to the arguments of the executions
+			execution.arguments().add(0, this.argument);
+			this.executions.add(execution);
+		}
+		if(!tree.executor.isEmpty()) {
+			this.executions.add(new Execution(List.of(this.argument, tree.argument), tree.executor));
+		}
+		return this;
+	}
+
+}
--- CommandAPI/commandapi-core/src/main/java/dev/jorel/commandapi/CommandAPICommand.java
+++ CommandAPI/commandapi-core/src/main/java/dev/jorel/commandapi/CommandAPICommand.java
@@ -24,10 +24,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Predicate;
-
-import org.bukkit.command.CommandSender;
 
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 
@@ -36,112 +32,28 @@ import dev.jorel.commandapi.arguments.IGreedyArgument;
 import dev.jorel.commandapi.arguments.MultiLiteralArgument;
 import dev.jorel.commandapi.exceptions.GreedyArgumentException;
 import dev.jorel.commandapi.exceptions.InvalidCommandNameException;
-import dev.jorel.commandapi.executors.CommandBlockCommandExecutor;
-import dev.jorel.commandapi.executors.CommandBlockResultingCommandExecutor;
-import dev.jorel.commandapi.executors.CommandExecutor;
-import dev.jorel.commandapi.executors.ConsoleCommandExecutor;
-import dev.jorel.commandapi.executors.ConsoleResultingCommandExecutor;
-import dev.jorel.commandapi.executors.EntityCommandExecutor;
-import dev.jorel.commandapi.executors.EntityResultingCommandExecutor;
-import dev.jorel.commandapi.executors.NativeCommandExecutor;
-import dev.jorel.commandapi.executors.NativeResultingCommandExecutor;
-import dev.jorel.commandapi.executors.PlayerCommandExecutor;
-import dev.jorel.commandapi.executors.PlayerResultingCommandExecutor;
-import dev.jorel.commandapi.executors.ProxyCommandExecutor;
-import dev.jorel.commandapi.executors.ProxyResultingCommandExecutor;
-import dev.jorel.commandapi.executors.ResultingCommandExecutor;
 
 /**
  * A builder used to create commands to be registered by the CommandAPI.
  */
-public class CommandAPICommand {
+public class CommandAPICommand extends ExecutableCommand<CommandAPICommand> {
 
-	private final String commandName;
-	
-	private CommandPermission permission = CommandPermission.NONE;
-	private String[] aliases = new String[0];
-	private Predicate<CommandSender> requirements = s -> true;
 	private List<Argument> args = new ArrayList<>();
 	private List<CommandAPICommand> subcommands = new ArrayList<>();
-	private CustomCommandExecutor<?> executor = new CustomCommandExecutor<>();
 	private boolean isConverted;
-	private Optional<String> shortDescription = Optional.empty();
-	private Optional<String> fullDescription = Optional.empty();
 	
 	/**
 	 * Creates a new command builder
 	 * @param commandName The name of the command to create
 	 */
 	public CommandAPICommand(String commandName) {
-		if(commandName == null || commandName.length() == 0) {
-			throw new InvalidCommandNameException(commandName);
-		}
-		
-		this.commandName = commandName;
+		super(commandName);
 		this.isConverted = false;
 	}
-	
-	/**
-	 * Applies a permission to the current command builder
-	 * @param permission The permission node required to execute this command
-	 * @return this command builder
-	 */
-	public CommandAPICommand withPermission(CommandPermission permission) {
-		this.permission = permission;
-		return this;
-	}
-	
-	/**
-	 * Applies a permission to the current command builder
-	 * @param permission The permission node required to execute this command
-	 * @return this command builder
-	 */
-	public CommandAPICommand withPermission(String permission) {
-		this.permission = CommandPermission.fromString(permission);
-		return this;
-	}
-	
-	/**
-	 * Applies a permission to the current command builder
-	 * @param permission The permission node required to execute this command
-	 * @return this command builder
-	 */
-	public CommandAPICommand withoutPermission(CommandPermission permission) {
-		this.permission = permission.negate();
-		return this;
-	}
-	
-	/**
-	 * Applies a permission to the current command builder
-	 * @param permission The permission node required to execute this command
-	 * @return this command builder
-	 */
-	public CommandAPICommand withoutPermission(String permission) {
-		this.permission = CommandPermission.fromString(permission).negate();
-		return this;
-	}
-	
-	/**
-	 * Adds a requirement that has to be satisfied to use this command. This method
-	 * can be used multiple times and each use of this method will AND its
-	 * requirement with the previously declared ones
-	 * 
-	 * @param requirement the predicate that must be satisfied to use this command
-	 * @return this command builder
-	 */
-	public CommandAPICommand withRequirement(Predicate<CommandSender> requirement) {
-		this.requirements = this.requirements.and(requirement);
-		return this;
-	}
-	
-	/**
-	 * Adds an array of aliases to the current command builder
-	 * @param aliases An array of aliases which can be used to execute this command
-	 * @return this command builder
-	 */
-	public CommandAPICommand withAliases(String... aliases) {
-		this.aliases = aliases;
-		return this;
+
+	protected CommandAPICommand(CommandMetaData metaData) {
+		super(metaData);
+		this.isConverted = false;
 	}
 	
 	/**
@@ -172,250 +84,6 @@ public class CommandAPICommand {
 	public CommandAPICommand withSubcommand(CommandAPICommand subcommand) {
 		this.subcommands.add(subcommand);
 		return this;
-	}
-	
-	/**
-	 * Sets the short description for this command. This is the help which is
-	 * shown in the main /help menu.
-	 * @param description the short description for this command
-	 * @return this command builder
-	 */
-	public CommandAPICommand withShortDescription(String description) {
-		this.shortDescription = Optional.ofNullable(description);
-		return this;
-	}
-	
-	/**
-	 * Sets the full description for this command. This is the help which is
-	 * shown in the specific /help page for this command (e.g. /help mycommand).
-	 * @param description the full description for this command
-	 * @return this command builder
-	 */
-	public CommandAPICommand withFullDescription(String description) {
-		this.fullDescription = Optional.ofNullable(description);
-		return this;
-	}
-	
-	/**
-	 * Sets the short and full description for this command. This is a short-hand
-	 * for the {@link CommandAPICommand#withShortDescription} and 
-	 * {@link CommandAPICommand#withFullDescription} methods.
-	 * @param shortDescription the short description for this command
-	 * @param fullDescription the full description for this command
-	 * @return this command builder
-	 */
-	public CommandAPICommand withHelp(String shortDescription, String fullDescription) {
-		this.shortDescription = Optional.ofNullable(shortDescription);
-		this.fullDescription = Optional.ofNullable(fullDescription);
-		return this;
-	}
-	
-	// Regular command executor 
-	
-	/**
-	 * Adds an executor to the current command builder
-	 * @param executor A lambda of type <code>(CommandSender, Object[]) -&gt; ()</code> that will be executed when the command is run
-	 * @return this command builder
-	 */
-	public CommandAPICommand executes(CommandExecutor executor) {
-		this.executor.addNormalExecutor(executor);
-		return this;
-	}
-	
-	/**
-	 * Adds an executor to the current command builder
-	 * @param executor A lambda of type <code>(CommandSender, Object[]) -&gt; int</code> that will be executed when the command is run
-	 * @return this command builder
-	 */
-	public CommandAPICommand executes(ResultingCommandExecutor executor) {
-		this.executor.addResultingExecutor(executor);
-		return this;
-	}
-	
-	// Player command executor
-	
-	/**
-	 * Adds an executor to the current command builder
-	 * @param executor A lambda of type <code>(Player, Object[]) -&gt; ()</code> that will be executed when the command is run
-	 * @return this command builder
-	 */
-	public CommandAPICommand executesPlayer(PlayerCommandExecutor executor) {
-		this.executor.addNormalExecutor(executor);
-		return this;
-	}
-	
-	/**
-	 * Adds an executor to the current command builder
-	 * @param executor A lambda of type <code>(Player, Object[]) -&gt; int</code> that will be executed when the command is run
-	 * @return this command builder
-	 */
-	public CommandAPICommand executesPlayer(PlayerResultingCommandExecutor executor) {
-		this.executor.addResultingExecutor(executor);
-		return this;
-	}
-	
-	// Entity command executor
-	
-	/**
-	 * Adds an executor to the current command builder
-	 * @param executor A lambda of type <code>(Entity, Object[]) -&gt; ()</code> that will be executed when the command is run
-	 * @return this command builder
-	 */
-	public CommandAPICommand executesEntity(EntityCommandExecutor executor) {
-		this.executor.addNormalExecutor(executor);
-		return this;
-	}
-	
-	/**
-	 * Adds an executor to the current command builder
-	 * @param executor A lambda of type <code>(Entity, Object[]) -&gt; int</code> that will be executed when the command is run
-	 * @return this command builder
-	 */
-	public CommandAPICommand executesEntity(EntityResultingCommandExecutor executor) {
-		this.executor.addResultingExecutor(executor);
-		return this;
-	}
-	
-	// Proxy command executor
-	
-	/**
-	 * Adds an executor to the current command builder
-	 * @param executor A lambda of type <code>(Entity, Object[]) -&gt; ()</code> that will be executed when the command is run
-	 * @return this command builder
-	 */
-	public CommandAPICommand executesProxy(ProxyCommandExecutor executor) {
-		this.executor.addNormalExecutor(executor);
-		return this;
-	}
-	
-	/**
-	 * Adds an executor to the current command builder
-	 * @param executor A lambda of type <code>(Entity, Object[]) -&gt; int</code> that will be executed when the command is run
-	 * @return this command builder
-	 */
-	public CommandAPICommand executesProxy(ProxyResultingCommandExecutor executor) {
-		this.executor.addResultingExecutor(executor);
-		return this;
-	}
-	
-	// Command block command sender
-	
-	/**
-	 * Adds an executor to the current command builder
-	 * @param executor A lambda of type <code>(BlockCommandSender, Object[]) -&gt; ()</code> that will be executed when the command is run
-	 * @return this command builder
-	 */
-	public CommandAPICommand executesCommandBlock(CommandBlockCommandExecutor executor) {
-		this.executor.addNormalExecutor(executor);
-		return this;
-	}
-	
-	/**
-	 * Adds an executor to the current command builder
-	 * @param executor A lambda of type <code>(BlockCommandSender, Object[]) -&gt; int</code> that will be executed when the command is run
-	 * @return this command builder
-	 */
-	public CommandAPICommand executesCommandBlock(CommandBlockResultingCommandExecutor executor) {
-		this.executor.addResultingExecutor(executor);
-		return this;
-	}
-	
-	// Console command sender
-	
-	/**
-	 * Adds an executor to the current command builder
-	 * @param executor A lambda of type <code>(BlockCommandSender, Object[]) -&gt; ()</code> that will be executed when the command is run
-	 * @return this command builder
-	 */
-	public CommandAPICommand executesConsole(ConsoleCommandExecutor executor) {
-		this.executor.addNormalExecutor(executor);
-		return this;
-	}
-	
-	/**
-	 * Adds an executor to the current command builder
-	 * @param executor A lambda of type <code>(BlockCommandSender, Object[]) -&gt; int</code> that will be executed when the command is run
-	 * @return this command builder
-	 */
-	public CommandAPICommand executesConsole(ConsoleResultingCommandExecutor executor) {
-		this.executor.addResultingExecutor(executor);
-		return this;
-	}
-	
-	/**
-	 * Adds an executor to the current command builder
-	 * @param executor A lambda of type <code>(NativeCommandExecutor, Object[]) -&gt; ()</code> that will be executed when the command is run
-	 * @return this command builder
-	 */
-	public CommandAPICommand executesNative(NativeCommandExecutor executor) {
-		this.executor.addNormalExecutor(executor);
-		return this;
-	}
-	
-	/**
-	 * Adds an executor to the current command builder
-	 * @param executor A lambda of type <code>(NativeCommandExecutor, Object[]) -&gt; int</code> that will be executed when the command is run
-	 * @return this command builder
-	 */
-	public CommandAPICommand executesNative(NativeResultingCommandExecutor executor) {
-		this.executor.addResultingExecutor(executor);
-		return this;
-	}
-	
-	/**
-	 * Returns the name of this command
-	 * @return the name of this command
-	 */
-	public String getName() {
-		return commandName;
-	}
-
-	/**
-	 * Returns the permission associated with this command
-	 * @return the permission associated with this command
-	 */
-	public CommandPermission getPermission() {
-		return permission;
-	}
-
-	/**
-	 * Sets the permission required to run this command
-	 * @param permission the permission required to run this command
-	 */
-	public void setPermission(CommandPermission permission) {
-		this.permission = permission;
-	}
-
-	/**
-	 * Returns an array of aliases that can be used to run this command
-	 * @return an array of aliases that can be used to run this command
-	 */
-	public String[] getAliases() {
-		return aliases;
-	}
-
-	/**
-	 * Sets the aliases for this command
-	 * @param aliases the aliases for this command
-	 */
-	public void setAliases(String[] aliases) {
-		this.aliases = aliases;
-	}
-
-	/**
-	 * Returns the requirements that must be satisfied to run this command
-	 * @return the requirements that must be satisfied to run this command
-	 */
-	public Predicate<CommandSender> getRequirements() {
-		return requirements;
-	}
-
-	/**
-	 * Sets the requirements that must be satisfied to run this command
-	 * @param requirements the requirements that must be satisfied to run this command
-	 */
-	public void setRequirements(Predicate<CommandSender> requirements) {
-		this.requirements = requirements;
 	}
 
 	/**
@@ -451,22 +119,6 @@ public class CommandAPICommand {
 	}
 
 	/**
-	 * Returns the executors that this command has
-	 * @return the executors that this command has
-	 */
-	public CustomCommandExecutor<? extends CommandSender> getExecutor() {
-		return executor;
-	}
-
-	/**
-	 * Sets the executors for this command
-	 * @param executor the executors for this command
-	 */
-	public void setExecutor(CustomCommandExecutor<? extends CommandSender> executor) {
-		this.executor = executor;
-	}
-
-	/**
 	 * Returns whether this command is an automatically converted command
 	 * @return whether this command is an automatically converted command
 	 */
@@ -481,12 +133,12 @@ public class CommandAPICommand {
 	//Expand subcommands into arguments
 	private void flatten(CommandAPICommand rootCommand, List<Argument> prevArguments, CommandAPICommand subcommand) {
 		
-		String[] literals = new String[subcommand.aliases.length + 1];
-		literals[0] = subcommand.commandName;
-		System.arraycopy(subcommand.aliases, 0, literals, 1, subcommand.aliases.length);
+		String[] literals = new String[subcommand.meta.aliases.length + 1];
+		literals[0] = subcommand.meta.commandName;
+		System.arraycopy(subcommand.meta.aliases, 0, literals, 1, subcommand.meta.aliases.length);
 		MultiLiteralArgument literal = (MultiLiteralArgument) new MultiLiteralArgument(literals)
-			.withPermission(subcommand.permission)
-			.withRequirement(subcommand.requirements)
+			.withPermission(subcommand.meta.permission)
+			.withRequirement(subcommand.meta.requirements)
 			.setListed(false);
 		
 		prevArguments.add(literal);
@@ -510,7 +162,7 @@ public class CommandAPICommand {
 	 */
 	public void register() {
 		if(!CommandAPI.canRegister()) {
-			CommandAPI.logWarning("Command /" + commandName + " is being registered after the server had loaded. Undefined behavior ahead!");
+			CommandAPI.logWarning("Command /" + meta.commandName + " is being registered after the server had loaded. Undefined behavior ahead!");
 		}
 		try {
 			Argument[] argumentsArr = args == null ? new Argument[0] : args.toArray(new Argument[0]);
@@ -527,12 +179,12 @@ public class CommandAPICommand {
 			//Assign the command's permissions to arguments if the arguments don't already have one
 			for(Argument argument : argumentsArr) {
 				if(argument.getArgumentPermission() == null) {
-					argument.withPermission(permission);
+					argument.withPermission(meta.permission);
 				}
 			}
 			
 			if(!executor.isEmpty()) {
-				CommandAPIHandler.getInstance().register(commandName, shortDescription, fullDescription, permission, aliases, requirements, argumentsArr, executor, isConverted);
+				CommandAPIHandler.getInstance().register( meta, argumentsArr, executor, isConverted);
 			}
 			
 			for(CommandAPICommand subcommand : this.subcommands) {
@@ -542,15 +194,6 @@ public class CommandAPICommand {
 			e.printStackTrace();
 		}
 		
-	}
-
-	/**
-	 * Overrides a command. Effectively the same as unregistering the command using
-	 * CommandAPI.unregister() and then registering the command using .register()
-	 */
-	public void override() {
-		CommandAPI.unregister(this.commandName, true);
-		register();
 	}
 	
 }
