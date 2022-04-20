dba8b7e867f2e34ec4f532c4ff8c61dd812f3302
Jorel Ali <github@jorel.dev>
2022-04-20 22:32:19 +0100

    Delete jitpacksetup.sh

--- jitpacksetup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "Old Java version:"
-java -version
-
-echo "Installing Java 16"
-wget https://github.com/sormuras/bach/raw/master/install-jdk.sh
-source install-jdk.sh --feature 16
-java -version
