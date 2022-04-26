adaef1ef20cbadd8d915c729c78b9a75f3d0b60c
Jorel Ali <github@jorel.dev>
2022-04-26 10:58:44 +0100

    Implement #265

--- CommandAPI/commandapi-core/src/main/java/dev/jorel/commandapi/exceptions/WrapperCommandSyntaxException.java
+++ CommandAPI/commandapi-core/src/main/java/dev/jorel/commandapi/exceptions/WrapperCommandSyntaxException.java
@@ -20,33 +20,116 @@
  *******************************************************************************/
 package dev.jorel.commandapi.exceptions;
 
+import java.io.PrintStream;
+import java.io.PrintWriter;
+
+import com.mojang.brigadier.Message;
+import com.mojang.brigadier.exceptions.CommandExceptionType;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 
 /**
- * A wrapper for the CommandSyntaxException so other developers don't have to import Mojang's brigadier
+ * A wrapper for the CommandSyntaxException so other developers don't have to
+ * import Mojang's brigadier
  */
 @SuppressWarnings("serial")
 public class WrapperCommandSyntaxException extends Exception {
-	
+
 	/**
 	 * The Brigadier CommandSyntaxException that this class wraps
 	 */
 	private CommandSyntaxException exception;
-	
+
 	/**
 	 * Creates a WrapperCommandSyntaxException
+	 * 
 	 * @param exception the exception to wrap
 	 */
 	public WrapperCommandSyntaxException(CommandSyntaxException exception) {
 		this.exception = exception;
 	}
-	
+
 	/**
 	 * Returns the wrapped CommandSyntaxException
+	 * 
 	 * @return the wrapped CommandSyntaxException
 	 */
 	public CommandSyntaxException getException() {
 		return this.exception;
+	}
+
+	// Overrides. Because this exception is effectively a CommandSyntaxException, we
+	// pass all Exception-related methods to this.exception
+
+	@Override
+	public synchronized Throwable fillInStackTrace() {
+		return this.exception.fillInStackTrace();
+	}
+
+	@Override
+	public String getMessage() {
+		return this.exception.getMessage();
+	}
+
+	@Override
+	public synchronized Throwable getCause() {
+		return this.exception.getCause();
+	}
+
+	@Override
+	public StackTraceElement[] getStackTrace() {
+		return this.exception.getStackTrace();
+	}
+
+	@Override
+	public synchronized Throwable initCause(Throwable cause) {
+		return this.exception.initCause(cause);
+	}
+
+	@Override
+	public String getLocalizedMessage() {
+		return this.exception.getLocalizedMessage();
+	}
+
+	@Override
+	public void printStackTrace() {
+		this.exception.printStackTrace();
+	}
+	
+	@Override
+	public void printStackTrace(PrintStream s) {
+		this.exception.printStackTrace(s);
+	}
+	
+	@Override
+	public void printStackTrace(PrintWriter s) {
+		this.exception.printStackTrace(s);
+	}
+	
+	@Override
+	public void setStackTrace(StackTraceElement[] stackTrace) {
+		this.exception.setStackTrace(stackTrace);
+	}
+
+	// CommandSyntaxException implemented methods
+
+	public Message getRawMessage() {
+		return this.exception.getRawMessage();
+	}
+
+	public String getContext() {
+		return this.exception.getContext();
+	}
+
+	public CommandExceptionType getType() {
+		return this.exception.getType();
+	}
+
+	public String getInput() {
+		return this.exception.getInput();
+	}
+
+	public int getCursor() {
+		return this.exception.getCursor();
 	}
 
 }
