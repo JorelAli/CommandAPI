b672dba9762321e5b6bb63812f1f0cdd3f5e243d
Jorel Ali <github@jorel.dev>
2022-04-29 12:56:43 +0100

    Adds a very important comment describing why we're using VarHandles for adding help topics

--- commandapi-1.18.2/src/main/java/dev/jorel/commandapi/nms/NMS_1_18_R2.java
+++ commandapi-1.18.2/src/main/java/dev/jorel/commandapi/nms/NMS_1_18_R2.java
@@ -1095,6 +1095,8 @@ public class NMS_1_18_R2 implements NMS<CommandSourceStack> {
 	@Override
 	public void addToHelpMap(Map<String, HelpTopic> helpTopicsToAdd) {
 		Map<String, HelpTopic> helpTopics = (Map<String, HelpTopic>) SimpleHelpMap_helpTopics.get(Bukkit.getServer().getHelpMap());
+		// We have to use VarHandles to use helpTopics.put (instead of .addTopic)
+		// becausewe're updating an existing help topic, not adding a new help topic
 		for(Map.Entry<String, HelpTopic> entry : helpTopicsToAdd.entrySet()) {
 			helpTopics.put(entry.getKey(), entry.getValue());
 		}
