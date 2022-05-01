d2c772b18e3b7cb96b496f4588d0032fdcdf440d
Jorel Ali <github@jorel.dev>
2022-05-01 21:20:04 +0100

    Remove buggy check for plugins to convert

--- commandapi-core/src/main/java/dev/jorel/commandapi/Config.java
+++ commandapi-core/src/main/java/dev/jorel/commandapi/Config.java
@@ -74,9 +74,11 @@ class Config {
 		this.skipSenderProxy = new ArrayList<>();
 		this.commandsToConvert = new ArrayList<>();
 
-		if(!fileConfig.getList("plugins-to-convert").isEmpty() && fileConfig.getMapList("plugins-to-convert").isEmpty()) {
-			CommandAPI.getLog().severe("plugins-to-convert has an invalid type. Did you miss a colon (:) after a plugin name?");
-		}
+		// TODO: This works for the specific case, but doesn't work for the general case. This should
+		// not trigger if there is a list of plugins (and no specific commands)
+//		if(!fileConfig.getList("plugins-to-convert").isEmpty() && fileConfig.getMapList("plugins-to-convert").isEmpty()) {
+//			CommandAPI.getLog().severe("plugins-to-convert has an invalid type. Did you miss a colon (:) after a plugin name?");
+//		}
 
 		for (Map<?, ?> map : fileConfig.getMapList("plugins-to-convert")) {
 			String[] pluginCommands;
