309fb628747c9c3187073e80a103031b196387c6
Jorel Ali <github@jorel.dev>
2022-05-11 11:57:47 +0100

    Adds JavaDocs for the ListArgument

--- commandapi-core/src/main/java/dev/jorel/commandapi/arguments/ListArgument.java
+++ commandapi-core/src/main/java/dev/jorel/commandapi/arguments/ListArgument.java
@@ -41,6 +41,10 @@ import dev.jorel.commandapi.IStringTooltip;
 import dev.jorel.commandapi.StringTooltip;
 import dev.jorel.commandapi.nms.NMS;
 
+/**
+ * An argument that accepts a list of objects
+ * @param <T> the type that this list argument generates a list of.
+ */
 @SuppressWarnings("rawtypes")
 public class ListArgument<T> extends Argument<Collection> implements IGreedyArgument {
 
@@ -153,16 +157,31 @@ public class ListArgument<T> extends Argument<Collection> implements IGreedyArgu
 		return list;
 	}
 
+	/**
+	 * A builder to create a ListArgument
+	 * @param <T> the type that the list argument generates a list of.
+	 */
 	public static class ListArgumentBuilder<T> {
 
 		private final String nodeName;
 		private final String delimiter;
 		private boolean allowDuplicates = false;
 
+		/**
+		 * Creates a new ListArgumentBuilder with a specified node name. Defaults the
+		 * delimiter for each element in the list to a space
+		 * 
+		 * @param nodeName the name of the node for this argument
+		 */
 		public ListArgumentBuilder(String nodeName) {
 			this(nodeName, " ");
 		}
 
+		/**
+		 * Creates a new ListArgumentBuilder with a specified node name
+		 * @param nodeName the name of the node for this argument
+		 * @param delimiter the separator for each element in the list (for example, a space or a comma)
+		 */
 		public ListArgumentBuilder(String nodeName, String delimiter) {
 			this.nodeName = nodeName;
 			this.delimiter = delimiter;
@@ -173,18 +192,41 @@ public class ListArgument<T> extends Argument<Collection> implements IGreedyArgu
 			return this;
 		}
 
+		/**
+		 * Specifies the list to use to generate suggestions for the list argument
+		 * 
+		 * @param list a function that accepts a CommandSender and returns a collection
+		 *             of elements to suggest for this list argument
+		 * @return this list argument builder
+		 */
 		public ListArgumentBuilderSuggests withList(Function<CommandSender, Collection<T>> list) {
 			return new ListArgumentBuilderSuggests(list);
 		}
 		
+		/**
+		 * Specifies the list to use to generate suggestions for the list argument
+		 * 
+		 * @param list a supplier that returns a collection of elements to suggest for
+		 *             this list argument
+		 * @return this list argument builder
+		 */
 		public ListArgumentBuilderSuggests withList(Supplier<Collection<T>> list) {
 			return withList(info -> list.get());
 		}
 
+		/**
+		 * Specifies the list to use to generate suggestions for the list argument
+		 * 
+		 * @param list a collection of elements to suggest for this list argument
+		 * @return this list argument builder
+		 */
 		public ListArgumentBuilderSuggests withList(Collection<T> list) {
 			return withList(info -> list);
 		}
 
+		/**
+		 * An intermediary class for the ListArgumentBuilder
+		 */
 		public class ListArgumentBuilderSuggests {
 
 			private final Function<CommandSender, Collection<T>> supplier;
@@ -193,18 +235,38 @@ public class ListArgument<T> extends Argument<Collection> implements IGreedyArgu
 				this.supplier = list;
 			}
 
+			/**
+			 * Specifies that the mapping function for this argument calls the <code>toString()</code> method.
+			 * @return this list argument builder
+			 */
 			public ListArgumentBuilderFinished withStringMapper() {
 				return withStringTooltipMapper(t -> StringTooltip.none(String.valueOf(t)));
 			}
 
+			/**
+			 * Specifies the mapping function of the specific type <code>T</code> to a
+			 * <code>String</code> so an element can be shown to a user as suggestions.
+			 * 
+			 * @return this list argument builder
+			 */
 			public ListArgumentBuilderFinished withMapper(Function<T, String> mapper) {
 				return withStringTooltipMapper(t -> StringTooltip.none(mapper.apply(t)));
 			}
 
+			/**
+			 * Specifies the mapping function of the specific type <code>T</code> to a
+			 * {@link IStringTooltip} so an element can be shown to a user as a suggestion
+			 * with a tooltip.
+			 * 
+			 * @return this list argument builder
+			 */
 			public ListArgumentBuilderFinished withStringTooltipMapper(Function<T, IStringTooltip> mapper) {
 				return new ListArgumentBuilderFinished(mapper);
 			}
 
+			/**
+			 * An intermediary class for the ListArgumentBuilder
+			 */
 			public class ListArgumentBuilderFinished {
 
 				private final Function<T, IStringTooltip> mapper;
@@ -213,6 +275,10 @@ public class ListArgument<T> extends Argument<Collection> implements IGreedyArgu
 					this.mapper = mapper;
 				}
 
+				/**
+				 * Builds this list argument.
+				 * @return a {@link ListArgument}
+				 */
 				public ListArgument<T> build() {
 					return new ListArgument<>(nodeName, delimiter, allowDuplicates, supplier, mapper);
 				}
