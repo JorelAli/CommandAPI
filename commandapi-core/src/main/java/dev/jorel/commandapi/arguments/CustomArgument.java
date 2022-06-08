a3a94f7820b8f110cdd9eec901022d8c22234add
Jorel Ali <github@jorel.dev>
2022-06-08 18:47:29 +0100

    Adds CustomArgument base arguments

--- commandapi-core/src/main/java/dev/jorel/commandapi/arguments/CustomArgument.java
+++ commandapi-core/src/main/java/dev/jorel/commandapi/arguments/CustomArgument.java
@@ -23,6 +23,7 @@ package dev.jorel.commandapi.arguments;
 import org.bukkit.command.CommandSender;
 
 import com.mojang.brigadier.LiteralMessage;
+import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
@@ -37,10 +38,9 @@ import dev.jorel.commandapi.nms.NMS;
  * @param <T> the return type of this argument
  */
 public class CustomArgument<T> extends Argument<T> {
-	
+
 	private CustomArgumentInfoParser<T> infoParser;
-	private boolean keyed;
-	
+
 	/**
 	 * Creates a CustomArgument with a valid parser, defaults to non-keyed argument
 	 * 
@@ -53,7 +53,7 @@ public class CustomArgument<T> extends Argument<T> {
 	public CustomArgument(String nodeName, CustomArgumentInfoParser<T> parser) {
 		this(nodeName, parser, false);
 	}
-	
+
 	/**
 	 * Creates a CustomArgument with a valid parser, defaults to non-keyed argument
 	 * 
@@ -62,21 +62,18 @@ public class CustomArgument<T> extends Argument<T> {
 	 *                 information such as the command sender, previously declared
 	 *                 arguments and current input. This parser should return an
 	 *                 object of your choice.
-	 * @param keyed     Whether this argument can accept Minecraft's
+	 * @param keyed    Whether this argument can accept Minecraft's
 	 *                 <code>NamespacedKey</code> as valid arguments
+	 * @deprecated Use {@link CustomArgument#CustomArgument(String, CustomArgumentInfoParser, Argument)} with {@link TextArgument} instead
 	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Deprecated
 	public CustomArgument(String nodeName, CustomArgumentInfoParser<T> parser, boolean keyed) {
-		super(nodeName, keyed ? CommandAPIHandler.getInstance().getNMS()._ArgumentMinecraftKeyRegistered() : StringArgumentType.string());
-		this.keyed = keyed;
-		this.infoParser = parser;
+		this(nodeName, parser, new DummyArgument(keyed ? StringArgumentType.string() : CommandAPIHandler.getInstance().getNMS()._ArgumentMinecraftKeyRegistered()));
 	}
-	
-	/**
-	 * Returns true if this argument is represented by a NamespacedKey
-	 * @return true if this argument is represented by a NamespacedKey
-	 */
-	public boolean isKeyed() {
-		return this.keyed;
+
+	public CustomArgument(String nodeName, CustomArgumentInfoParser<T> parser, Argument<?> base) {
+		super(nodeName, base.getRawType());
 	}
 
 	@Override
@@ -88,21 +85,16 @@ public class CustomArgument<T> extends Argument<T> {
 	public CommandAPIArgumentType getArgumentType() {
 		return CommandAPIArgumentType.CUSTOM;
 	}
-	
+
 	@Override
 	public <CommandListenerWrapper> T parseArgument(NMS<CommandListenerWrapper> nms,
-			CommandContext<CommandListenerWrapper> cmdCtx, String key, Object[] previousArgs) throws CommandSyntaxException {
-		String customresult;
-		if(this.keyed) {
-			customresult = nms.getKeyedAsString(cmdCtx, key);
-		} else {
-			customresult = cmdCtx.getArgument(key, String.class);
-		}
-		
+			CommandContext<CommandListenerWrapper> cmdCtx, String key, Object[] previousArgs)
+			throws CommandSyntaxException {
+		final String customresult = CommandAPIHandler.getRawArgumentInput(cmdCtx, key);
+
 		try {
-			CustomArgumentInfo info = new CustomArgumentInfo(nms.getCommandSenderFromCSS(cmdCtx.getSource()), previousArgs, 
-					customresult);
-			return infoParser.apply(info);
+			return infoParser.apply(new CustomArgumentInfo(nms.getCommandSenderFromCSS(cmdCtx.getSource()),
+					previousArgs, customresult));
 		} catch (CustomArgumentException e) {
 			throw e.toCommandSyntax(customresult, cmdCtx);
 		} catch (Exception e) {
@@ -114,64 +106,73 @@ public class CustomArgument<T> extends Argument<T> {
 			}).create();
 		}
 	}
-	
+
 	/**
 	 * MessageBuilder is used to create error messages for invalid argument inputs
 	 */
 	public static class MessageBuilder {
 		StringBuilder builder;
-		
+
 		/**
 		 * Create a blank message
 		 */
 		public MessageBuilder() {
 			builder = new StringBuilder();
 		}
-		
+
 		/**
 		 * Create a message with an input string
+		 * 
 		 * @param str The string to start the message with
 		 */
 		public MessageBuilder(String str) {
 			builder = new StringBuilder(str);
 		}
-		
+
 		/**
 		 * Appends the argument input that the CommandSender used in this command.<br>
-		 * For example, if <code>/foo bar</code> was executed and an error occurs
-		 * with the CustomArgument <code>bar</code>, then the arg input will append
-		 * <code>bar</code> to the end of the message.<br><br> This input is determined
-		 * at runtime, and is stored as <code>%input%</code> until executed 
+		 * For example, if <code>/foo bar</code> was executed and an error occurs with
+		 * the CustomArgument <code>bar</code>, then the arg input will append
+		 * <code>bar</code> to the end of the message.<br>
+		 * <br>
+		 * This input is determined at runtime, and is stored as <code>%input%</code>
+		 * until executed
+		 * 
 		 * @return A reference to this object
 		 */
 		public MessageBuilder appendArgInput() {
 			builder.append("%input%");
 			return this;
 		}
-		
+
 		/**
 		 * Appends the whole input that the CommandSender used in this command.<br>
-		 * For example, if <code>/foo bar</code> was executed, then <code>foo bar</code> will be
-		 * appended to the end of the message.<br><br> This input is determined
-		 * at runtime, and is stored as <code>%finput%</code> until executed 
+		 * For example, if <code>/foo bar</code> was executed, then <code>foo bar</code>
+		 * will be appended to the end of the message.<br>
+		 * <br>
+		 * This input is determined at runtime, and is stored as <code>%finput%</code>
+		 * until executed
+		 * 
 		 * @return A reference to this object
 		 */
 		public MessageBuilder appendFullInput() {
 			builder.append("%finput%");
 			return this;
 		}
-		
+
 		/**
 		 * Appends <code>&lt;--[HERE]</code> to the end of the message
+		 * 
 		 * @return A reference to this object
 		 */
 		public MessageBuilder appendHere() {
 			builder.append("<--[HERE]");
 			return this;
 		}
-		
+
 		/**
 		 * Appends a string to the end of this message
+		 * 
 		 * @param str The string to append to the end of this message
 		 * @return A reference to this object
 		 */
@@ -179,9 +180,10 @@ public class CustomArgument<T> extends Argument<T> {
 			builder.append(str);
 			return this;
 		}
-		
+
 		/**
 		 * Appends an object to the end of this message
+		 * 
 		 * @param obj The object to append to the end of this message
 		 * @return A reference to this object
 		 */
@@ -189,9 +191,10 @@ public class CustomArgument<T> extends Argument<T> {
 			builder.append(obj);
 			return this;
 		}
-		
+
 		/**
 		 * Returns the String content of this MessageBuilder
+		 * 
 		 * @return the String content of this MessageBuilder
 		 */
 		@Override
@@ -199,7 +202,7 @@ public class CustomArgument<T> extends Argument<T> {
 			return builder.toString();
 		}
 	}
-	
+
 	/**
 	 * An exception used to create command-related errors for the CustomArgument
 	 */
@@ -208,44 +211,53 @@ public class CustomArgument<T> extends Argument<T> {
 
 		final String errorMessage;
 		final MessageBuilder errorMessageBuilder;
-		
+
 		/**
 		 * Constructs a CustomArgumentException with a given error message
-		 * @param errorMessage the error message to display to the user when this exception is thrown
+		 * 
+		 * @param errorMessage the error message to display to the user when this
+		 *                     exception is thrown
 		 */
 		public CustomArgumentException(String errorMessage) {
 			this.errorMessage = errorMessage;
 			this.errorMessageBuilder = null;
 		}
-		
+
 		/**
 		 * Constructs a CustomArgumentException with a given error message
-		 * @param errorMessage the error message to display to the user when this exception is thrown
+		 * 
+		 * @param errorMessage the error message to display to the user when this
+		 *                     exception is thrown
 		 */
 		public CustomArgumentException(MessageBuilder errorMessage) {
 			this.errorMessage = null;
 			this.errorMessageBuilder = errorMessage;
 		}
-		
+
 		/**
-		 * Converts this CustomArgumentException into a CommandSyntaxException 
-		 * @param result the argument that the user entered that caused this exception to arise
+		 * Converts this CustomArgumentException into a CommandSyntaxException
+		 * 
+		 * @param result the argument that the user entered that caused this exception
+		 *               to arise
 		 * @param cmdCtx the command context that executed this command
 		 * @return a Brigadier CommandSyntaxException
 		 */
 		public CommandSyntaxException toCommandSyntax(String result, CommandContext<?> cmdCtx) {
-			if(errorMessage == null) {
-				//Deal with MessageBuilder
-				String errorMsg = errorMessageBuilder.toString().replace("%input%", result).replace("%finput%", cmdCtx.getInput());
-				return new SimpleCommandExceptionType(() -> {return errorMsg;}).create();
+			if (errorMessage == null) {
+				// Deal with MessageBuilder
+				String errorMsg = errorMessageBuilder.toString().replace("%input%", result).replace("%finput%",
+						cmdCtx.getInput());
+				return new SimpleCommandExceptionType(() -> {
+					return errorMsg;
+				}).create();
 			} else {
-				//Deal with String
+				// Deal with String
 				return new SimpleCommandExceptionType(new LiteralMessage(errorMessage)).create();
 			}
 		}
-		
+
 	}
-	
+
 	/**
 	 * A record which contains information which can be passed to the custom
 	 * argument's parser.
@@ -257,12 +269,9 @@ public class CustomArgument<T> extends Argument<T> {
 	 * @param input        the current input which the user has typed for this
 	 *                     argument
 	 */
-	public record CustomArgumentInfo(
-		CommandSender sender, 
-		Object[] previousArgs, 
-		String input) {
+	public record CustomArgumentInfo(CommandSender sender, Object[] previousArgs, String input) {
 	}
-	
+
 	/**
 	 * A FunctionalInterface that takes in a {@link CustomArgumentInfo}, returns T
 	 * and can throw a {@link CustomArgumentException}
@@ -271,7 +280,7 @@ public class CustomArgument<T> extends Argument<T> {
 	 */
 	@FunctionalInterface
 	public static interface CustomArgumentInfoParser<T> {
-		
+
 		/**
 		 * Applies a CustomArgumentInfo input to this custom argument parser
 		 * 
@@ -281,4 +290,29 @@ public class CustomArgument<T> extends Argument<T> {
 		 */
 		public T apply(CustomArgumentInfo info) throws CustomArgumentException;
 	}
-}
+	
+	@Deprecated
+	private static class DummyArgument<D> extends Argument<D> {
+		
+		private DummyArgument(ArgumentType<?> type) {
+			super(null, type);
+		}
+
+		@Override
+		public Class<D> getPrimitiveType() {
+			return null;
+		}
+
+		@Override
+		public CommandAPIArgumentType getArgumentType() {
+			return null;
+		}
+
+		@Override
+		public <CommandSourceStack> D parseArgument(NMS<CommandSourceStack> nms,
+				CommandContext<CommandSourceStack> cmdCtx, String key, Object[] previousArgs)
+				throws CommandSyntaxException {
+			return null;
+		}
+	}
+} 
