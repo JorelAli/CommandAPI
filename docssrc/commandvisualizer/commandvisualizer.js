e48db6d7737b026cd3fd2d21d7f8237d9d6ad573
Jorel Ali <github@jorel.dev>
2022-08-31 10:59:07 +0100

    Refactor command visualizer, implement Minecraft-style styling text

--- docssrc/commandvisualizer/commandvisualizer.js
+++ docssrc/commandvisualizer/commandvisualizer.js
@@ -1,93 +1,170 @@
 "use strict";
 
-document.getElementById("cmd-input").oninput = function() {
+/** @type HTMLElement */
+const commandInput = document.getElementById("cmd-input");
+const errorMessageBox = document.getElementById("error-box");
+const suggestionsBox = document.getElementById("suggestions-box");
 
-	/** @type HTMLElement */
-	const commandInput = this;
-	const errorMessageBox = document.getElementById("error-box");
-	const suggestionsBox = document.getElementById("suggestions-box");
+/**
+ * Gets the current cursor position.
+ *
+ * From https://thewebdev.info/2022/04/24/how-to-get-contenteditable-caret-position-with-javascript/
+ * @returns The current cursor position for the current element
+ */
+function getCursorPosition() {
+	const sel = document.getSelection();
+	sel.modify("extend", "backward", "paragraphboundary");
+	const pos = sel.toString().length;
+	if (sel.anchorNode !== undefined && sel.anchorNode !== null) {
+		sel.collapseToEnd();
+	}
+	return pos;
+};
 
-	/**
-	 * Gets the current cursor position.
-	 *
-	 * From https://thewebdev.info/2022/04/24/how-to-get-contenteditable-caret-position-with-javascript/
-	 * @returns The current cursor position for the current element
-	 */
-	const cursorPosition = () => {
-		const sel = document.getSelection();
-		sel.modify("extend", "backward", "paragraphboundary");
-		const pos = sel.toString().length;
-		if (sel.anchorNode !== undefined && sel.anchorNode !== null) {
-			sel.collapseToEnd();
-		}
-		return pos;
-	};
-
-	/**
-	 * Sets the current cursor position. This has to take into account the fact
-	 * that the current element is made up of many HTML elements instead of
-	 * plaintext, so selection ranges have to span across those elements to find
-	 * the exact location you're looking for.
-	 *
-	 * From https://stackoverflow.com/a/41034697/4779071
-	 * @param {number} index the number of characters into the current element
-	 * to place the cursor at
-	 */
-	function setCurrentCursorPosition(index) {
-		if (index >= 0) {
-			const createRange = (/** @type Node */ node, /** @type {{count: number}} */ chars, /** @type Range */ range) => {
-				if (!range) {
-					range = document.createRange();
-					range.selectNode(node);
-					range.setStart(node, 0);
-				}
-			
-				if (chars.count === 0) {
-					range.setEnd(node, chars.count);
-				} else if (node && chars.count > 0) {
-					if (node.nodeType === Node.TEXT_NODE) {
-						if (node.textContent.length < chars.count) {
-							chars.count -= node.textContent.length;
-						} else {
-							range.setEnd(node, chars.count);
-							chars.count = 0;
-						}
+/**
+ * Sets the current cursor position. This has to take into account the fact
+ * that the current element is made up of many HTML elements instead of
+ * plaintext, so selection ranges have to span across those elements to find
+ * the exact location you're looking for.
+ *
+ * From https://stackoverflow.com/a/41034697/4779071
+ * @param {number} index the number of characters into the current element
+ *                       to place the cursor at
+ * @param {Node} element the element to set the cursor for
+ */
+function setCursorPosition(index, element) {
+	if (index >= 0) {
+		const createRange = (/** @type Node */ node, /** @type {{count: number}} */ chars, /** @type Range */ range) => {
+			if (!range) {
+				range = document.createRange();
+				range.selectNode(node);
+				range.setStart(node, 0);
+			}
+		
+			if (chars.count === 0) {
+				range.setEnd(node, chars.count);
+			} else if (node && chars.count > 0) {
+				if (node.nodeType === Node.TEXT_NODE) {
+					if (node.textContent.length < chars.count) {
+						chars.count -= node.textContent.length;
 					} else {
-						for (let lp = 0; lp < node.childNodes.length; lp++) {
-							range = createRange(node.childNodes[lp], chars, range);
-			
-							if (chars.count === 0) {
-								break;
-							}
+						range.setEnd(node, chars.count);
+						chars.count = 0;
+					}
+				} else {
+					for (let lp = 0; lp < node.childNodes.length; lp++) {
+						range = createRange(node.childNodes[lp], chars, range);
+		
+						if (chars.count === 0) {
+							break;
 						}
 					}
-				} 
-			
-				return range;
-			};
-	
-			// We wrap index in an object so that recursive calls can use the
-			// "new" value which is updated inside the object itself
-			let range = createRange(commandInput, { count: index });
-	
-			if (range) {
-				range.collapse(false);
-				let selection = window.getSelection();
-				selection.removeAllRanges();
-				selection.addRange(range);
-			}
+				}
+			} 
+		
+			return range;
+		};
+
+		// We wrap index in an object so that recursive calls can use the
+		// "new" value which is updated inside the object itself
+		let range = createRange(element, { count: index });
+
+		if (range) {
+			range.collapse(false);
+			let selection = window.getSelection();
+			selection.removeAllRanges();
+			selection.addRange(range);
+		}
+	}
+};
+
+function getSelectedSuggestion() {
+	return document.querySelector(".yellow");
+}
+
+const ChatColor = {
+	BLACK: "\u00A70",
+	DARK_BLUE: "\u00A71",
+	DARK_GREEN: "\u00A72",
+	DARK_AQUA: "\u00A73",
+	DARK_RED: "\u00A74",
+	DARK_PURPLE: "\u00A75",
+	GOLD: "\u00A76",
+	GRAY: "\u00A77",
+	DARK_GRAY: "\u00A78",
+	BLUE: "\u00A79",
+	GREEN: "\u00A7a",
+	AQUA: "\u00A7b",
+	RED: "\u00A7c",
+	LIGHT_PURPLE: "\u00A7d",
+	YELLOW: "\u00A7e",
+	WHITE: "\u00A7f",
+}
+
+const ChatColorCSS = {
+	"0": "black",
+	"1": "dark_blue",
+	"2": "dark_green",
+	"3": "dark_aqua",
+	"4": "dark_red",
+	"5": "dark_purple",
+	"6": "gold",
+	"7": "gray",
+	"8": "dark_gray",
+	"9": "blue",
+	"a": "green",
+	"b": "aqua",
+	"c": "red",
+	"d": "light_purple",
+	"e": "yellow",
+	"f": "white"
+}
+
+/**
+ * Takes Minecraft text and renders it in the chat box
+ */
+function setText(minecraftCodedText) {
+	let buffer = "";
+	let currentColor = "";
+
+	function writeBuffer() {
+		if(buffer.length > 0) {
+			let elem = document.createElement("span");
+			elem.className = currentColor;
+			elem.innerText = buffer;
+			commandInput.appendChild(elem);
+			buffer = "";
 		}
 	};
 
-	let cursorPos = cursorPosition();
+	for(let i = 0; i < minecraftCodedText.length; i++) {
+		if(minecraftCodedText[i] === "\u00A7") {
+			writeBuffer();
+			currentColor = ChatColorCSS[minecraftCodedText[i + 1]];
+			i++;
+			continue;
+		} else {
+			buffer += minecraftCodedText[i];
+		}
+	}
+
+	writeBuffer();
+}
+
+document.getElementById("cmd-input").oninput = function() {
+	let cursorPos = getCursorPosition();
 	let commands = ["say", "tp", "w", "weather", "whitelist", "worldborder"];
 
 	/** @type string */
 	let rawText = commandInput.innerText;
 
+	// Reset the text
+	commandInput.innerHTML = "";
+
 	// Command forward slash
 	let element = document.createElement("span");
 	element.innerText = "/";
+	commandInput.appendChild(element);
 
 	let errorText = "";
 	let suggestions = [];
@@ -100,34 +177,15 @@ document.getElementById("cmd-input").oninput = function() {
 		const rawArgs = rawText.split(" ").slice(1);
 
 		if(!(commands.some((x) => x === command))) {
-			// The command is invalid (the command doesn't exist). Make the
-			// whole text red.
-			let redText = document.createElement("span");
-			redText.className = "red";
-			redText.innerText = rawTextNoSlash;
-			element.append(redText);
-
+			// The command is invalid (the command doesn't exist). Make the whole text red.
+			setText(ChatColor.RED + rawTextNoSlash);
 			errorText = "Unknown or incomplete command, see below for error at position 1: /<--[HERE]";
 		} else {
-			// Make the command gray
-			const rawTextElements = rawText.split(" ");
-			let grayText = document.createElement("span");
-			grayText.innerText = rawTextElements[0].slice(1); // Don't include the /
-			element.append(grayText);
-
-			// Make everything else cyan (for now)
-			let cyanText = document.createElement("span");
-			cyanText.className = "cyan";
-			cyanText.innerText = " " + rawTextElements.slice(1).join(" ");
-			element.append(cyanText);
+			setText(command + " " + ChatColor.AQUA + rawArgs.join(" "));
 		}
 
 		suggestions = commands.filter((x) => x.startsWith(rawTextNoSlash) && x !== rawTextNoSlash);
 	}
-
-	// Reset the text and add the new text.
-	commandInput.innerHTML = "";
-	commandInput.appendChild(element);
 
 	// Set the cursor back to where it was. Since commands always start with a
 	// forward slash, the only possible "starting caret position" is position 1
@@ -135,7 +193,7 @@ document.getElementById("cmd-input").oninput = function() {
 	if(cursorPos === 0) {
 		cursorPos = 1;
 	}
-	setCurrentCursorPosition(cursorPos);
+	setCursorPosition(cursorPos, document.getElementById("cmd-input"));
 	commandInput.focus();
 
 	// If any errors appear, display them
@@ -203,6 +261,8 @@ document.getElementById("cmd-input").addEventListener('keydown', (evt) => {
 								suggestionsBox.childNodes[i - 1].className = "yellow";
 							}
 						}
+
+						window.dispatchEvent(new Event("suggestionsUpdated"));
 						
 						break;
 					}
@@ -213,6 +273,23 @@ document.getElementById("cmd-input").addEventListener('keydown', (evt) => {
 		default:
 			// console.log(evt.key)
 			break;
+	}
+});
+
+window.addEventListener("suggestionsUpdated", (event) => {
+	/** @type string */
+	let rawText = commandInput.innerText;
+
+	let errorText = "";
+	let suggestions = [];
+
+	if(!suggestionsBox.hidden) {
+		/** @type string */
+		let selectedSuggestionText = getSelectedSuggestion().innerText.trim();
+		// TODO: This obviously needs to be specific to the current suggestions, not the whole input
+		if(rawText !== selectedSuggestionText) {
+
+		}
 	}
 });
 
