52f00ecb84f3f0be3d0237eb837b2607c391db18
BuildTools <willkroboth@gmail.com>
2022-10-27 09:52:21 -0400

    Fix Javadocs for CommandAPIVersionHandler

--- commandapi-core/src/main/java/dev/jorel/commandapi/CommandAPIVersionHandler.java
+++ commandapi-core/src/main/java/dev/jorel/commandapi/CommandAPIVersionHandler.java
@@ -1,9 +1,7 @@
 package dev.jorel.commandapi;
 
-import dev.jorel.commandapi.arguments.AbstractArgument;
-
 /**
- * This file handles loadeding the correct platform implementation. The CommandAPIVersionHandler
+ * This file handles loading the correct platform implementation. The CommandAPIVersionHandler
  * file within the commandapi-core module is NOT used at run time. Instead, the platform modules
  * replace this class with their own version that handles loads the correct class for thier version
  */
@@ -12,12 +10,9 @@ public interface CommandAPIVersionHandler {
 	/**
 	 * Returns an instance of the version's implementation of AbstractPlatform.
 	 *
-	 * @param <Argument>      the implementation of AbstractArgument
-	 * @param <CommandSender> the command sender type
-	 * @param <Source>        the command source type
-	 * @return an instance of AbstractPlatform which can run on the specified server version
+	 * @return an instance of AbstractPlatform which can run on the currently active server
 	 */
-	static <Argument extends AbstractArgument<?, ?, Argument, CommandSender>, CommandSender, Source> AbstractPlatform<Argument, CommandSender, Source> getPlatform() {
+	static AbstractPlatform<?, ?, ?> getPlatform() {
 		throw new RuntimeException("You have the wrong copy of the CommandAPI! If you're shading, did you use commandapi-core instead of commandapi-{platform}-shade?");
 	}
 }
--- commandapi-platforms/commandapi-bukkit/commandapi-bukkit-plugin-test/src/main/java/dev/jorel/commandapi/CommandAPIVersionHandler.java
+++ commandapi-platforms/commandapi-bukkit/commandapi-bukkit-plugin-test/src/main/java/dev/jorel/commandapi/CommandAPIVersionHandler.java
@@ -4,7 +4,7 @@ import dev.jorel.commandapi.nms.NMS_1_19_1_R1;
 import dev.jorel.commandapi.test.MockNMS;
 
 /**
- * This file handles loadeding the correct platform implementation. The CommandAPIVersionHandler
+ * This file handles loading the correct platform implementation. The CommandAPIVersionHandler
  * file within the commandapi-core module is NOT used at run time. Instead, the platform modules
  * replace this class with their own version that handles loads the correct class for thier version
  */
