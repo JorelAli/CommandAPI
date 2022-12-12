135a4a10c58b53e4664fe6b2fdf320a0f056fb9d
Jorel Ali <github@jorel.dev>
2022-12-12 16:34:48 +0000

    Generate all of 8.7.0 documentation. Update homepage to include 8.7.0 links

--- docs/javadocs/html/classdev_1_1jorel_1_1commandapi_1_1arguments_1_1_sound_argument.js
+++ docs/javadocs/html/classdev_1_1jorel_1_1commandapi_1_1arguments_1_1_sound_argument.js
@@ -1,8 +1,8 @@
 var classdev_1_1jorel_1_1commandapi_1_1arguments_1_1_sound_argument =
 [
-    [ "SoundArgument", "classdev_1_1jorel_1_1commandapi_1_1arguments_1_1_sound_argument.html#a3442bcfe2fe95265cf0ba5214bbc106e", null ],
-    [ "SoundArgument", "classdev_1_1jorel_1_1commandapi_1_1arguments_1_1_sound_argument.html#a2399df44cebf6e6b9a5b4cc4a23c4dc2", null ],
-    [ "getSuggestionProvider", "classdev_1_1jorel_1_1commandapi_1_1arguments_1_1_sound_argument.html#a8aad8895a5b65feaa9c44fb3b57674f9", null ],
+    [ "NamespacedKey", "classdev_1_1jorel_1_1commandapi_1_1arguments_1_1_sound_argument_1_1_namespaced_key.html", "classdev_1_1jorel_1_1commandapi_1_1arguments_1_1_sound_argument_1_1_namespaced_key" ],
+    [ "SoundArgument", "classdev_1_1jorel_1_1commandapi_1_1arguments_1_1_sound_argument.html#ab8be7c9a8ed34e96a55246c20b022a8e", null ],
+    [ "getSuggestionProvider", "classdev_1_1jorel_1_1commandapi_1_1arguments_1_1_sound_argument.html#aec90f5fabb29215fd51f75e5372aa72f", null ],
     [ "includeSafeSuggestions", "classdev_1_1jorel_1_1commandapi_1_1arguments_1_1_sound_argument.html#a0a8af4f677c1c736bc10a08f5ef6c420", null ],
     [ "includeWithSafeSuggestions", "classdev_1_1jorel_1_1commandapi_1_1arguments_1_1_sound_argument.html#a0c172462db17f842589e288f43dde4a6", null ],
     [ "includeWithSafeSuggestionsT", "classdev_1_1jorel_1_1commandapi_1_1arguments_1_1_sound_argument.html#a53be4c945c22ab14c2ba68c35c185230", null ],
--- docs/javadocs/html/search/classes_c.js
+++ docs/javadocs/html/search/classes_c.js
@@ -2,5 +2,7 @@ var searchData=
 [
   ['objectiveargument_0',['ObjectiveArgument',['../classdev_1_1jorel_1_1commandapi_1_1arguments_1_1_objective_argument.html',1,'dev::jorel::commandapi::arguments']]],
   ['objectivecriteriaargument_1',['ObjectiveCriteriaArgument',['../classdev_1_1jorel_1_1commandapi_1_1arguments_1_1_objective_criteria_argument.html',1,'dev::jorel::commandapi::arguments']]],
-  ['offlineplayerargument_2',['OfflinePlayerArgument',['../classdev_1_1jorel_1_1commandapi_1_1arguments_1_1_offline_player_argument.html',1,'dev::jorel::commandapi::arguments']]]
+  ['offlineplayerargument_2',['OfflinePlayerArgument',['../classdev_1_1jorel_1_1commandapi_1_1arguments_1_1_offline_player_argument.html',1,'dev::jorel::commandapi::arguments']]],
+  ['oneentity_3',['OneEntity',['../classdev_1_1jorel_1_1commandapi_1_1arguments_1_1_entity_selector_argument_1_1_one_entity.html',1,'dev::jorel::commandapi::arguments::EntitySelectorArgument']]],
+  ['oneplayer_4',['OnePlayer',['../classdev_1_1jorel_1_1commandapi_1_1arguments_1_1_entity_selector_argument_1_1_one_player.html',1,'dev::jorel::commandapi::arguments::EntitySelectorArgument']]]
 ];
