8f339cb0c6757e6ebdc1860ad19760adea308ccd
Jorel Ali <github@jorel.dev>
2022-12-12 13:41:09 +0000

    Remove JavaDoc complaints in commandapi-plugin

--- commandapi-plugin/src/main/java/dev/jorel/commandapi/exceptions/UnknownArgumentException.java
+++ commandapi-plugin/src/main/java/dev/jorel/commandapi/exceptions/UnknownArgumentException.java
@@ -26,6 +26,10 @@ package dev.jorel.commandapi.exceptions;
 @SuppressWarnings("serial")
 public class UnknownArgumentException extends Exception {
 	
+	/**
+	 * An exception caused when an unknown argument type is provided
+	 * @param argument the argument that wasn't recognised
+	 */
 	public UnknownArgumentException(String argument) {
 		super("The argument type '" + argument + "' is not recognized!");
 	}
