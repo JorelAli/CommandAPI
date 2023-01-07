7e54622e76de2a2488c5675a85c154fdfb19416f
DerEchtePilz <tlfm90@gmail.com>
2023-01-07 12:21:57 +0100

    removes an unused import in AbstractCommandAPICommand

--- commandapi-core/src/main/java/dev/jorel/commandapi/AbstractCommandAPICommand.java
+++ commandapi-core/src/main/java/dev/jorel/commandapi/AbstractCommandAPICommand.java
@@ -29,7 +29,6 @@ import dev.jorel.commandapi.exceptions.OptionalArgumentException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.function.Predicate;
 
