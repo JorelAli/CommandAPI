fd5b6f9434223bcad95a6ee28abab8760067d779
DerEchtePilz <tlfm90@gmail.com>
2023-01-10 17:11:18 +0100

    Adds FloatArgument tests

--- commandapi-platforms/commandapi-bukkit/commandapi-bukkit-plugin-test/src/test/java/dev/jorel/commandapi/test/arguments/ArgumentPrimitiveTests.java
+++ commandapi-platforms/commandapi-bukkit/commandapi-bukkit-plugin-test/src/test/java/dev/jorel/commandapi/test/arguments/ArgumentPrimitiveTests.java
@@ -2,6 +2,7 @@ package dev.jorel.commandapi.test.arguments;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import dev.jorel.commandapi.arguments.FloatArgument;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -163,6 +164,80 @@ public class ArgumentPrimitiveTests extends TestBase {
 		assertCommandFailsWith(player, "test 30", "Integer must not be more than 20, found 30 at position 5: test <--[HERE]");
 	}
 
-	// TODO: Double, Float, Long
+	// TODO: Double, Long
+	@Test
+	public void executionTestWithFloatArgument() {
+		Mut<Float> results = Mut.of();
+
+		new CommandAPICommand("test")
+			.withArguments(new FloatArgument("value"))
+			.executesPlayer((player, args) -> {
+				results.set((float) args.get(0));
+			})
+			.register();
+
+		PlayerMock player = server.addPlayer();
+
+		// /test 10
+		server.dispatchCommand(player, "test 10.0");
+		assertEquals(10.0F, results.get());
+
+		// /test -10
+		server.dispatchCommand(player, "test -10.0");
+		assertEquals(-10.0F, results.get());
+
+		// /test 0
+		server.dispatchCommand(player, "test 0.0");
+		assertEquals(0.0F, results.get());
+
+		// /test Float.MAX_VALUE
+		// String floatMaxValue = String.valueOf(Float.MAX_VALUE - 1.0F);
+		// server.dispatchCommand(player, "test " + floatMaxValue);
+		// assertEquals(Float.MAX_VALUE - 1.0F, results.get());
+
+		// /test -Float.MAX_VALUE
+		// String floatMinValue = String.valueOf(-Float.MAX_VALUE);
+		// server.dispatchCommand(player, "test " + floatMinValue);
+		// assertEquals(-Float.MAX_VALUE, results.get());
+
+		// /test 123hello
+		assertCommandFailsWith(player, "test 123hello", "Expected whitespace to end one argument, but found trailing data at position 8: test 123<--[HERE]");
+
+		// /test hello123
+		assertCommandFailsWith(player, "test hello123", "Expected float at position 5: test <--[HERE]");
+	}
+
+	@Test
+	public void executionTestWithFloatBoundedArgument() {
+		Mut<Float> results = Mut.of();
+
+		new CommandAPICommand("test")
+			.withArguments(new FloatArgument("value", 10.0F, 20.0F))
+			.executesPlayer((player, args) -> {
+				results.set((Float) args.get(0));
+			})
+			.register();
+
+		PlayerMock player = server.addPlayer();
+
+		// /test 10
+		server.dispatchCommand(player, "test 10.0");
+		assertEquals(10.0F, results.get());
+
+		// /test 15
+		server.dispatchCommand(player, "test 15.0");
+		assertEquals(15.0F, results.get());
+
+		// /test 20
+		server.dispatchCommand(player, "test 20.0");
+		assertEquals(20.0F, results.get());
+
+		// /test 0
+		assertCommandFailsWith(player, "test 0", "Float must not be less than 10.0, found 0.0 at position 5: test <--[HERE]");
+
+		// /test 30
+		assertCommandFailsWith(player, "test 30", "Float must not be more than 20.0, found 30.0 at position 5: test <--[HERE]");
+
+	}
 	
 }
