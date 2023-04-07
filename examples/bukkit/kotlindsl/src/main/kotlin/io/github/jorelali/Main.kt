32e26951b0c23e0cb65bd399ba9f1b95f6d564f2
Lukas Planz <lukas.planz@web.de>
2023-04-07 17:06:03 +0200

    Remove code that has to be added only when shading the entire CommandAPI

--- examples/bukkit/kotlindsl/src/main/kotlin/io/github/jorelali/Main.kt
+++ examples/bukkit/kotlindsl/src/main/kotlin/io/github/jorelali/Main.kt
@@ -1,15 +1,10 @@
 package io.github.jorelali
 
-import dev.jorel.commandapi.CommandAPI
-import dev.jorel.commandapi.CommandAPIBukkitConfig
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
 
 	override fun onEnable() {
-		CommandAPI.onLoad(CommandAPIBukkitConfig(this))
-		CommandAPI.onEnable()
 		SayHelloCommand().register()
 	}
-
 }
