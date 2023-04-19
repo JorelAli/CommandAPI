a1f6b55daa1b6818cc697701768937fdf0319235
Jorel Ali <github@jorel.dev>
2023-04-19 13:32:22 +0100

    Update ArgumentRotationTests.java

--- commandapi-platforms/commandapi-bukkit/commandapi-bukkit-test/commandapi-bukkit-test-tests/src/test/java/dev/jorel/commandapi/test/arguments/ArgumentRotationTests.java
+++ commandapi-platforms/commandapi-bukkit/commandapi-bukkit-test/commandapi-bukkit-test-tests/src/test/java/dev/jorel/commandapi/test/arguments/ArgumentRotationTests.java
@@ -2,10 +2,14 @@ package dev.jorel.commandapi.test.arguments;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
+
+import org.bukkit.Location;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import be.seeseemelk.mockbukkit.WorldMock;
 import be.seeseemelk.mockbukkit.entity.PlayerMock;
 import dev.jorel.commandapi.CommandAPICommand;
 import dev.jorel.commandapi.arguments.RotationArgument;
@@ -74,105 +78,33 @@ class ArgumentRotationTests extends TestBase {
 		}
 
 		{
-			// TODO: Come back to this, it's broken af
-//			player.setLocation(new Location(new WorldMock(), 2, 2, 2, 75, 135));
-//			
-//			// /test ~ ~
-//			server.dispatchCommand(player, "test ~ ~");
-//			Rotation rotation = results.get();
-//			assertEquals(75.0f, rotation.getYaw());
-//			assertEquals(135.0f, rotation.getPitch());
+			player.setLocation(new Location(new WorldMock(), 2, 2, 2, 75, 135));
+
+			// /test ~ ~
+			server.dispatchCommand(player, "test ~ ~");
+			Rotation rotation = results.get();
+			assertEquals(75.0f, rotation.getYaw());
+			assertEquals(135.0f, rotation.getPitch());
 		}
 
 		assertNoMoreResults(results);
 	}
 
-//	@Test
-//	public void executionTestWithRotationArgumentAllRotations() {
-//		Mut<Rotation> results = Mut.of();
-//
-//		new CommandAPICommand("test")
-//			.withArguments(new RotationArgument("potion"))
-//			.executesPlayer((player, args) -> {
-//				results.set((Rotation) args.get("potion"));
-//			})
-//			.register();
-//
-//		PlayerMock player = server.addPlayer();
-//
-//		for (Rotation Rotation : Rotation.values()) {
-//			server.dispatchCommand(player, "test " + Rotation.getKey().getKey());
-//			assertEquals(Rotation, results.get());
-//		}
-//
-//		assertNoMoreResults(results);
-//	}
-
 	/********************
 	 * Suggestion tests *
 	 ********************/
 
-//	@Test
-//	public void suggestionTestWithRotationArgument() {
-//		new CommandAPICommand("test")
-//			.withArguments(new RotationArgument("potion"))
-//			.executesPlayer((player, args) -> {
-//			})
-//			.register();
-//
-//		PlayerMock player = server.addPlayer();
-//
-//		// /test minecraft:
-//		assertEquals(List.of(
-//			"minecraft:absorption",
-//			"minecraft:bad_omen",
-//			"minecraft:blindness",
-//			"minecraft:conduit_power",
-//			"minecraft:darkness",
-//			"minecraft:dolphins_grace",
-//			"minecraft:fire_resistance",
-//			"minecraft:glowing",
-//			"minecraft:haste",
-//			"minecraft:health_boost",
-//			"minecraft:hero_of_the_village",
-//			"minecraft:hunger",
-//			"minecraft:instant_damage",
-//			"minecraft:instant_health",
-//			"minecraft:invisibility",
-//			"minecraft:jump_boost",
-//			"minecraft:levitation",
-//			"minecraft:luck",
-//			"minecraft:mining_fatigue",
-//			"minecraft:nausea",
-//			"minecraft:night_vision",
-//			"minecraft:poison",
-//			"minecraft:regeneration",
-//			"minecraft:resistance",
-//			"minecraft:saturation",
-//			"minecraft:slow_falling",
-//			"minecraft:slowness",
-//			"minecraft:speed",
-//			"minecraft:strength",
-//			"minecraft:unluck",
-//			"minecraft:water_breathing",
-//			"minecraft:weakness",
-//			"minecraft:wither"), server.getSuggestions(player, "test minecraft:"));
-//
-//		// /test minecraft:s
-//		assertEquals(List.of(
-//			"minecraft:saturation",
-//			"minecraft:slow_falling",
-//			"minecraft:slowness",
-//			"minecraft:speed",
-//			"minecraft:strength"), server.getSuggestions(player, "test minecraft:s"));
-//
-//		// /test s
-//		assertEquals(List.of(
-//			"minecraft:saturation",
-//			"minecraft:slow_falling",
-//			"minecraft:slowness",
-//			"minecraft:speed",
-//			"minecraft:strength"), server.getSuggestions(player, "test s"));
-//	}
+	@Test
+	public void suggestionTestWithRotationArgument() {
+		new CommandAPICommand("test")
+			.withArguments(new RotationArgument("rotation"))
+			.executesPlayer(P_EXEC)
+			.register();
+
+		PlayerMock player = server.addPlayer();
+
+		// /test
+		assertEquals(List.of(), server.getSuggestions(player, "test "));
+	}
 
 }
