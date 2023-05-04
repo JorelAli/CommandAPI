f08ec92ff9272910bf9da27daf2ab04d9b39ee29
DerEchtePilz <tlfm90@gmail.com>
2023-05-04 15:11:33 +0200

    Update remaining mentions of getOrDefault to say getOptional

--- commandapi-documentation-code/src/main/java/dev/jorel/commandapi/examples/java/Examples.java
+++ commandapi-documentation-code/src/main/java/dev/jorel/commandapi/examples/java/Examples.java
@@ -1637,7 +1637,7 @@ new CommandAPICommand("rate")
         int rating = (int) args.get("rating");
 
         // The target player is optional, so give it a default here
-        CommandSender target = (CommandSender) args.getOrDefault("target", sender);
+        CommandSender target = (CommandSender) args.getOptional("target").orElse(sender);
 
         target.sendMessage("Your " + topic + " was rated: " + rating + "/10");
     })
