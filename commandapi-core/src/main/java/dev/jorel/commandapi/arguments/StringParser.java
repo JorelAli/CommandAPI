35b1286e9bc56242036bc14e2347ae8f0da61fab
BuildTools <willkroboth@gmail.com>
2023-06-05 11:56:55 -0400

    Add javadocs to StringParser

--- commandapi-core/src/main/java/dev/jorel/commandapi/arguments/StringParser.java
+++ commandapi-core/src/main/java/dev/jorel/commandapi/arguments/StringParser.java
@@ -2,7 +2,18 @@ package dev.jorel.commandapi.arguments;
 
 import dev.jorel.commandapi.exceptions.WrapperCommandSyntaxException;
 
+/**
+ * A class that represents a function that parses a String into another object. See {@link StringParser#parse(String)}
+ * @param <T> The class that this {@link StringParser} turns strings into.
+ */
 @FunctionalInterface
 public interface StringParser<T> {
+	/**
+	 * A method that turns a String into an object of type T.
+	 *
+	 * @param s The String to parse
+	 * @return The resulting parsed object
+	 * @throws WrapperCommandSyntaxException If there is a problem with the syntax of the String that prevents it from being turned into an object of type T.
+	 */
 	T parse(String s) throws WrapperCommandSyntaxException;
 }
