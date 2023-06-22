4a4279e2b2be1d6f00f82688ff39b76e58e8af0b
Jorel Ali <github@jorel.dev>
2023-06-22 21:30:38 +0100

    Revert "Yeet the commandapi-plugin-test code into oblivion, we (probably) don't need it anymore"
    This reverts commit f4fcf2f407a929db253b693815c0a0fd2f2b1abe.

---
+++ commandapi-plugin-test/src/main/java/dev/jorel/commandapi/test/CommandAPITestCommand.java
@@ -0,0 +1,69 @@
+package dev.jorel.commandapi.test;
+
+import java.util.ListIterator;
+
+import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.CustomCommandExecutor;
+import dev.jorel.commandapi.executors.IExecutorNormal;
+import dev.jorel.commandapi.executors.IExecutorResulting;
+
+public class CommandAPITestCommand extends CommandAPICommand {
+
+	@SuppressWarnings("rawtypes")
+	private final ArgumentInspector listener;
+
+	public CommandAPITestCommand(String commandName) {
+		this(commandName, null);
+	}
+
+	public CommandAPITestCommand(String commandName, @SuppressWarnings("rawtypes") ArgumentInspector listener) {
+		super(commandName);
+		this.listener = listener;
+	}
+	
+	// Replace all executors with our 'testing' executor
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private void mapExecutor(CustomCommandExecutor customCommandExecutor) {
+		// Do normal executors
+		{
+			ListIterator it = customCommandExecutor.getNormalExecutors().listIterator();
+			while(it.hasNext()) {
+				IExecutorNormal executor = (IExecutorNormal) it.next();
+				IExecutorNormal newExecutor = (sender, args) -> {
+					for(Object arg : args) {
+						listener.set(arg);
+					}
+					executor.executeWith(sender, args);
+				};
+				it.set(newExecutor);
+			}
+		}
+
+		// And now do resulting executors
+		{
+			ListIterator it = customCommandExecutor.getResultingExecutors().listIterator();
+			while(it.hasNext()) {
+				IExecutorResulting executor = (IExecutorResulting) it.next();
+				IExecutorResulting newExecutor = (sender, args) -> {
+					for(Object arg : args) {
+						listener.set(arg);
+					}
+					return executor.executeWith(sender, args);
+				};
+				it.set(newExecutor);
+			}
+		}
+	}
+
+	@Override
+	public void register() {
+		if(this.listener != null) {
+			mapExecutor(this.executor);
+			for (CommandAPICommand subcommand : this.getSubcommands()) {
+				mapExecutor(subcommand.getExecutor());
+			}
+		}
+		super.register();
+	}
+
+}
