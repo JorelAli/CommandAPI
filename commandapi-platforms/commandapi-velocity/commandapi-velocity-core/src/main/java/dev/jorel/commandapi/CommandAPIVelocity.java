f773fdaa3a638805a28b31a2afb6a576464151f5
BuildTools <46540330+willkroboth@users.noreply.github.com>
2023-06-26 15:46:13 -0400

    Address Sonar Cloud code smells in CommandAPIVelocity

--- commandapi-platforms/commandapi-velocity/commandapi-velocity-core/src/main/java/dev/jorel/commandapi/CommandAPIVelocity.java
+++ commandapi-platforms/commandapi-velocity/commandapi-velocity-core/src/main/java/dev/jorel/commandapi/CommandAPIVelocity.java
@@ -34,6 +34,8 @@ import java.util.Collection;
 import java.util.List;
 
 public class CommandAPIVelocity implements CommandAPIPlatform<Argument<?>, CommandSource, CommandSource> {
+
+	// References to utility classes
 	private static CommandAPIVelocity instance;
 	private static InternalVelocityConfig config;
 
@@ -41,11 +43,11 @@ public class CommandAPIVelocity implements CommandAPIPlatform<Argument<?>, Comma
 	private CommandDispatcher<CommandSource> dispatcher;
 
 	public CommandAPIVelocity() {
-		instance = this;
+		CommandAPIVelocity.instance = this;
 	}
 
 	public static CommandAPIVelocity get() {
-		if(instance != null) {
+		if (instance != null) {
 			return instance;
 		} else {
 			throw new IllegalStateException("Tried to access CommandAPIVelocity instance, but it was null! Are you using CommandAPI features before calling CommandAPI#onLoad?");
@@ -53,7 +55,7 @@ public class CommandAPIVelocity implements CommandAPIPlatform<Argument<?>, Comma
 	}
 
 	public static InternalVelocityConfig getConfiguration() {
-		if(config != null) {
+		if (config != null) {
 			return config;
 		} else {
 			throw new IllegalStateException("Tried to access InternalVelocityConfig, but it was null! Did you load the CommandAPI properly with CommandAPI#onLoad?");
@@ -62,30 +64,38 @@ public class CommandAPIVelocity implements CommandAPIPlatform<Argument<?>, Comma
 
 	@Override
 	public void onLoad(CommandAPIConfig<?> config) {
-		if(config instanceof CommandAPIVelocityConfig velocityConfig) {
-			CommandAPIVelocity.config = new InternalVelocityConfig(velocityConfig);
+		if (config instanceof CommandAPIVelocityConfig velocityConfig) {
+			CommandAPIVelocity.setInternalConfig(new InternalVelocityConfig(velocityConfig));
 		} else {
 			CommandAPI.logError("CommandAPIVelocity was loaded with non-Velocity config!");
 			CommandAPI.logError("Attempts to access Velocity-specific config variables will fail!");
 		}
 
 		commandManager = getConfiguration().getServer().getCommandManager();
-		{
-			Field dispatcherField = CommandAPIHandler.getField(commandManager.getClass(), "dispatcher");
-			try {
-				dispatcher = (CommandDispatcher<CommandSource>) dispatcherField.get(commandManager);
-			} catch (Exception ignored) {
-				CommandAPI.logError("Could not access Velocity's Brigadier CommandDispatcher");
-			}
+
+		// We can't use a SafeVarHandle here because we don't have direct access to the
+		//  `com.velocitypowered.proxy.command.VelocityCommandManager` class that holds the field.
+		//  Instead, the class comes from `commandManager.getClass()`
+		Field dispatcherField = CommandAPIHandler.getField(commandManager.getClass(), "dispatcher");
+		try {
+			dispatcher = (CommandDispatcher<CommandSource>) dispatcherField.get(commandManager);
+		} catch (Exception ignored) {
+			CommandAPI.logError("Could not access Velocity's Brigadier CommandDispatcher");
 		}
+	}
+
+	private static void setInternalConfig(InternalVelocityConfig internalVelocityConfig) {
+		CommandAPIVelocity.config = internalVelocityConfig;
 	}
 
 	@Override
 	public void onEnable() {
+		// Nothing to do
 	}
 
 	@Override
 	public void onDisable() {
+		// Nothing to do
 	}
 
 	@Override
@@ -170,9 +180,9 @@ public class CommandAPIVelocity implements CommandAPIPlatform<Argument<?>, Comma
 	public VelocityCommandSender<? extends CommandSource> getCommandSenderFromCommandSource(CommandSource cs) {
 		// Given a Brigadier CommandContext source (result of CommandContext.getSource),
 		// we need to convert that to an AbstractCommandSender.
-		if(cs instanceof ConsoleCommandSource ccs)
+		if (cs instanceof ConsoleCommandSource ccs)
 			return new VelocityConsoleCommandSender(ccs);
-		if(cs instanceof Player p)
+		if (cs instanceof Player p)
 			return new VelocityPlayer(p);
 		throw new IllegalArgumentException("Unknown CommandSource: " + cs);
 	}
@@ -213,7 +223,7 @@ public class CommandAPIVelocity implements CommandAPIPlatform<Argument<?>, Comma
 
 	@Override
 	public void reloadDataPacks() {
-		// TODO Auto-generated method stub
+		// Nothing to do, Velocity does not have data packs
 	}
 
 	@Override
