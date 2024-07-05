313c299bd3d34f4f7eda5727bdd3f2ae7dc1d411
BuildTools <46540330+willkroboth@users.noreply.github.com>
2024-07-05 13:42:57 -0400

    Add `assertCommandSucceeds` and `assertCommandFails` methods to CommandAPITestUtilities
    Add DispatchCommandUtilitiesTests to fully test these methods
    
    Add CommandTestBase to provide utilities when testing the test toolkit
    
    Enable Jacoco code coverage report for `commandapi-test-toolkit`
    
    Add some comments

---
+++ commandapi-platforms/commandapi-bukkit/commandapi-bukkit-test-toolkit/src/test/java/dev/jorel/commandapi/CommandTestBase.java
@@ -0,0 +1,38 @@
+package dev.jorel.commandapi;
+
+import be.seeseemelk.mockbukkit.MockBukkit;
+import be.seeseemelk.mockbukkit.ServerMock;
+import com.google.errorprone.annotations.CanIgnoreReturnValue;
+import org.junit.jupiter.api.function.Executable;
+import org.opentest4j.AssertionFailedError;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public abstract class CommandTestBase extends CommandAPITestUtilities {
+	// Setup
+	protected ServerMock server;
+	protected MockCommandAPIPlugin plugin;
+
+	public void setUp() {
+		server = MockBukkit.mock();
+		plugin = MockBukkit.load(MockCommandAPIPlugin.class);
+	}
+
+	public void tearDown() {
+		MockBukkit.unmock();
+	}
+
+	// Additional test utilities
+	@CanIgnoreReturnValue
+	public <T extends Throwable> T assertThrowsWithMessage(Class<T> exceptionClass, Executable executable, String expectedMessage) {
+		T exception = assertThrows(exceptionClass, executable);
+		assertEquals(expectedMessage, exception.getMessage());
+		return exception;
+	}
+
+	@CanIgnoreReturnValue
+	public AssertionFailedError assertAssertionFails(Executable executable, String expectedMessage) {
+		return assertThrowsWithMessage(AssertionFailedError.class, executable, expectedMessage);
+	}
+}
