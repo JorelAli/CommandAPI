d9c1f0d6a85cd94ab7d2d20682c12ad20684ea54
willkroboth <46540330+willkroboth@users.noreply.github.com>
2024-08-09 20:17:46 -0400

    Proof of concept for one Paper plugin for all server versions
    Tested and working on Paper without noticeable problems.
    
    `commandapi-(paper/spigot)-(shade/plugin)-mojang-mapped` modules removed. Spigot shade/plugin are now only spigot-mapped since Spigot servers are always spigot-mapped. Paper shade/plugin contain spigot-mapped nms for pre 1.20.5 and mojang-mapped nms for 1.20.5+, which is compatible with official Paper server jar downloads.
    
    Paper 1.20.5/6 and 1.21 relocate the mojang-mapped nms common classes they need to a separate package to avoid conflicting with the spigot-mapped versions. The CommandAPI Paper plugin (and plugins that shade Paper CommandAPI) marks itself as mojang-mapped so the Paper server does not remap these classes. Mojang-mapped classes that get remapped do not work.
    
    On Paper, MojangMappedVersionHandler now has to return true/false based on the detected server version.
    
    Also made sure Brigadier wasn't shaded into the Spigot jar, and the preprocessor classes weren't shaded into Paper and Spigot.
    
    Further exploration:
    - This 9-year-old Maven bug https://github.com/apache/maven/pull/69 is annoying. If you build the paper-plugin module individually, it gets shaded correctly. However, if you build the whole project, extra classes get included and mess up the build. For example, the un-relocated version of classes from `commandapi-bukkit-1.21:mojang-mapped` get included transitively through `commandapi-paper-1.21`, even though that dependency is not present in the dependency reduced pom because they are supposed to be relocated by the shade plugin. Perhaps there is a better way to set things up so this can be avoided (I wonder if gradle has this issue :P).
    - These changes require plugins that shade `commandapi-paper-shade` to mark their plugins as mojang-mapped. This would have to be mentioned in the docs. Also, it may be possible that a developer would for some reason have to mark their plugins as spigot-mapped. In this case, we may want to provide a `commandapi-paper-shade-spigot-mapped` module, so that 1.20.5+ nms can be properly passed though Paper's automatic remapping.
    - Currently on `dev/dev`, `commandapi-bukkit-plugin` and `commandapi-bukkit-shade` support Spigot and Paper in one jar. On `dev/commandapi-paper`, this is no longer the case. It seems fine to do this for the plugins, since a server owner can just drop in the correct jar. However, for plugins that shade the CommandAPI, if they want to support both Spigot and Paper, they are currently forced to produce two jars as well. Perhaps there could be a `commandapi-bukkit-shade` module with a CommandAPIVersionHandler that loads Spigot or Paper nms based on the detected server version. This would give shade-based developers back the ability to create a single jar that supports both Spigot and Paper.
    - Other things mentioned in added comments

--- commandapi-platforms/commandapi-paper/commandapi-paper-mojang-mapped/src/main/java/dev/jorel/commandapi/MojangMappedVersionHandler.java
+++ commandapi-platforms/commandapi-paper/commandapi-paper-mojang-mapped/src/main/java/dev/jorel/commandapi/MojangMappedVersionHandler.java
@@ -1,12 +1,36 @@
 package dev.jorel.commandapi;
 
+import dev.jorel.commandapi.exceptions.UnsupportedVersionException;
+import org.bukkit.Bukkit;
+
 /**
  * This file handles loading the correct mappings information. The MojangMappedVersionHandler
- * file within the commandapi-core module may not be used at run time. Instead, if a platform module
- * wants to be mojang-mapped, they can replace it with this class where {@link #isMojangMapped()} returns {@code true}.
+ * file within the commandapi-core module is not used on Paper, since Paper 1.20.5+ uses mojang mappings.
  */
 public interface MojangMappedVersionHandler {
     static boolean isMojangMapped() {
-        return true;
+		// TODO: Is it possible to consider `shouldUseLatestNMSVersion` here?
+		//  Currently, the configuration isn't yet loaded when this method is called for the first time by
+		//  SafeVarHandle, since CommandAPIHandler's static initializer uses SafeVarHandle for reflection:
+		//    at dev.jorel.commandapi.CommandAPIHandler.<clinit>(CommandAPIHandler.java:87)
+		//    at dev.jorel.commandapi.CommandAPI.onDisable(CommandAPI.java:168)
+		//    at dev.jorel.commandapi.CommandAPI.<clinit>(CommandAPI.java:23)
+		//    at dev.jorel.commandapi.CommandAPIMain.onLoad(CommandAPIMain.java:66)
+		//  I *think* it's okay to assume mojang-mappings for an unknown version. When CommandAPIVersionHandler is used,
+		//  it can properly use `shouldUseLatestNMSVersion` and throw UnsupportedVersionException if needed.
+//		if (CommandAPI.getConfiguration().shouldUseLatestNMSVersion()) {
+//			// 1.21
+//			return true;
+//		}
+		String version = Bukkit.getBukkitVersion().split("-")[0];
+
+		// TODO: Inspired by CommandAPIVersionHandler, but probably a better way to do this...
+		return switch (version) {
+			case "1.16.5", "1.17", "1.17.1", "1.18", "1.18.1", "1.18.2", "1.19", "1.19.1", "1.19.2", "1.19.3",
+				 "1.19.4", "1.20", "1.20.1", "1.20.2", "1.20.3", "1.20.4" -> false;
+			case "1.20.5", "1.20.6", "1.21" -> true;
+//			default -> throw new UnsupportedVersionException(version);
+			default -> true;
+		};
     }
 }
