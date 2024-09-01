65eb2589a74d9f1a99b487be2a7996bbe66551f7
willkroboth <46540330+willkroboth@users.noreply.github.com>
2024-09-01 13:39:22 -0400

    Remove test commands

--- commandapi-platforms/commandapi-bukkit/commandapi-bukkit-plugin-common/src/main/java/dev/jorel/commandapi/CommandAPIMain.java
+++ commandapi-platforms/commandapi-bukkit/commandapi-bukkit-plugin-common/src/main/java/dev/jorel/commandapi/CommandAPIMain.java
@@ -20,19 +20,14 @@
  *******************************************************************************/
 package dev.jorel.commandapi;
 
-import dev.jorel.commandapi.arguments.*;
 import dev.jorel.commandapi.config.BukkitConfigurationAdapter;
-import dev.jorel.commandapi.executors.CommandArguments;
-import dev.jorel.commandapi.wrappers.IntegerRange;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.InvalidPluginException;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -138,122 +133,6 @@ public class CommandAPIMain extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		CommandAPI.onEnable();
-
-		List<String> tags = new ArrayList<>();
-		tags.add("hello");
-		tags.add("world");
-
-		new CommandTree("servertag")
-			.then(
-				new LiteralArgument("add").then(
-					new StringArgument("tag").executes(info -> {
-						String tag = info.args().getUnchecked("tag");
-
-						tags.add(tag);
-					})
-				)
-			)
-			.then(
-				new LiteralArgument("tag").then(
-					new DynamicMultiLiteralArgument("tag", sender -> {
-						if (sender instanceof Player player) {
-							List<String> addPlayer = new ArrayList<>(tags);
-							addPlayer.add(player.getName());
-							return addPlayer;
-						} else {
-							return tags;
-						}
-					}).then(new IntegerArgument("extra").replaceSafeSuggestions(SafeSuggestions.suggest(1, 2, 3))
-						.executes(info -> {
-								String tag = info.args().getUnchecked("tag");
-								int extra = info.args().getUnchecked("extra");
-
-								info.sender().sendMessage(tag + " " + extra);
-							}
-						)
-					))
-			)
-			.register();
-
-		new CommandAPICommand("updateCommands")
-			.executes(info -> {
-				for (Player player : Bukkit.getOnlinePlayers()) {
-					CommandAPI.updateRequirements(player);
-				}
-			})
-			.register();
-
-		// example from https://github.com/JorelAli/CommandAPI/issues/483
-		new CommandAPICommand("serverFlag")
-			.withArguments(
-				new FlagsArgument("filters")
-					.loopingBranch(
-						new LiteralArgument("filter", "sort").setListed(true),
-						new MultiLiteralArgument("sortType", "furthest", "nearest", "random")
-					)
-					.loopingBranch(
-						new LiteralArgument("filter", "limit").setListed(true),
-						new IntegerArgument("limitAmount", 0)
-					)
-					.loopingBranch(
-						new LiteralArgument("filter", "distance").setListed(true),
-						new IntegerRangeArgument("range")
-					)
-			)
-			.executes(info -> {
-				for (CommandArguments branch : info.args().<List<CommandArguments>>getUnchecked("filters")) {
-					String filterType = branch.getUnchecked("filter");
-					info.sender().sendMessage(switch (filterType) {
-						case "sort" -> "Sort " + branch.<String>getUnchecked("sortType");
-						case "limit" -> "Limit " + branch.<Integer>getUnchecked("limitAmount");
-						case "distance" -> "Distance " + branch.<IntegerRange>getUnchecked("range");
-						default -> "Unknown branch " + filterType;
-					});
-				}
-			})
-			.register();
-
-		List<String> keys = new ArrayList<>();
-		List<String> values = new ArrayList<>();
-		new CommandTree("dynamicMap")
-			.then(
-				new LiteralArgument("add").then(
-					new MultiLiteralArgument("type", "key", "value").then(
-						new StringArgument("item").executes(info -> {
-							List<String> choice = (info.args().getUnchecked("type").equals("key")) ?
-								keys :
-								values;
-
-							String item = info.args().getUnchecked("item");
-							choice.add(item);
-						})
-					)
-				)
-			)
-			.then(
-				new CustomArgument<>(
-					new FlagsArgument("map").loopingBranch(
-						new DynamicMultiLiteralArgument("key", sender -> keys),
-						new LiteralArgument("->"), // Haha! Multi-character delimiter :P
-						new DynamicMultiLiteralArgument("value", sender -> values)
-					),
-					info -> {
-						Map<String, String> result = new HashMap<>();
-						for (CommandArguments args : (List<CommandArguments>) info.currentInput()) {
-							String key = args.getUnchecked("key");
-							String value = args.getUnchecked("value");
-
-							if (result.put(key, value) != null)
-								throw CustomArgument.CustomArgumentException.fromString("Duplicate key \"" + key + "\"");
-						}
-						return result;
-					}
-				).executes(info -> {
-					Map<String, String> result = info.args().getUnchecked("map");
-					info.sender().sendMessage(result.toString());
-				})
-			)
-			.register();
 	}
 
 	/**
--- commandapi-platforms/commandapi-velocity/commandapi-velocity-plugin/src/main/java/dev/jorel/commandapi/CommandAPIMain.java
+++ commandapi-platforms/commandapi-velocity/commandapi-velocity-plugin/src/main/java/dev/jorel/commandapi/CommandAPIMain.java
@@ -7,12 +7,8 @@ import com.velocitypowered.api.event.proxy.ProxyReloadEvent;
 import com.velocitypowered.api.event.proxy.ProxyShutdownEvent;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.plugin.annotation.DataDirectory;
-import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
-import dev.jorel.commandapi.arguments.*;
 import dev.jorel.commandapi.config.VelocityConfigurationAdapter;
-import dev.jorel.commandapi.executors.CommandArguments;
-import net.kyori.adventure.text.Component;
 import org.spongepowered.configurate.ConfigurationNode;
 import org.spongepowered.configurate.yaml.NodeStyle;
 import org.spongepowered.configurate.yaml.YamlConfigurationLoader;
@@ -20,8 +16,6 @@ import org.spongepowered.configurate.yaml.YamlConfigurationLoader;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Logger;
 
 /**
@@ -73,74 +67,6 @@ public class CommandAPIMain {
 	public void onProxyInitialization(ProxyInitializeEvent event) {
 		// Enable
 		CommandAPI.onEnable();
-
-		List<String> tags = new ArrayList<>();
-		tags.add("hello");
-		tags.add("world");
-
-		new CommandTree("proxytag")
-			.then(
-				new LiteralArgument("add").then(
-					new StringArgument("tag").executes(info -> {
-						String tag = info.args().getUnchecked("tag");
-
-						tags.add(tag);
-					})
-				)
-			)
-			.then(
-				new LiteralArgument("tag").then(
-					new DynamicMultiLiteralArgument("tag", sender -> {
-						if (sender instanceof Player player) {
-							List<String> addPlayer = new ArrayList<>(tags);
-							addPlayer.add(player.getUsername());
-							return addPlayer;
-						} else {
-							return tags;
-						}
-					}).then(new IntegerArgument("extra").replaceSafeSuggestions(SafeSuggestions.suggest(1, 2, 3))
-						.executes(info -> {
-							String tag = info.args().getUnchecked("tag");
-							int extra = info.args().getUnchecked("extra");
-
-							info.sender().sendMessage(Component.text(tag + " " + extra));
-						})
-					)
-				)
-			)
-			.register();
-
-		// example from https://github.com/JorelAli/CommandAPI/issues/483
-		new CommandAPICommand("proxyflag")
-			.withArguments(
-				new FlagsArgument("filters")
-					.loopingBranch(
-						new LiteralArgument("filter", "sort").setListed(true),
-						new MultiLiteralArgument("sortType", "furthest", "nearest", "random")
-					)
-					.loopingBranch(
-						new LiteralArgument("filter", "limit").setListed(true),
-						new IntegerArgument("limitAmount", 0)
-					)
-					.loopingBranch(
-						new LiteralArgument("filter", "distance").setListed(true),
-						new IntegerArgument("low"),
-						new IntegerArgument("high")
-					)
-			)
-			.executes(info -> {
-				for (CommandArguments branch : info.args().<List<CommandArguments>>getUnchecked("filters")) {
-					String filterType = branch.getUnchecked("filter");
-					info.sender().sendMessage(Component.text(switch (filterType) {
-						case "sort" -> "Sort " + branch.<String>getUnchecked("sortType");
-						case "limit" -> "Limit " + branch.<Integer>getUnchecked("limitAmount");
-						case "distance" -> "Distance " + branch.<Integer>getUnchecked("low")
-										+ " to " + branch.<Integer>getUnchecked("high");
-						default -> "Unknown branch " + filterType;
-					}));
-				}
-			})
-			.register();
 	}
 
 	@Subscribe
