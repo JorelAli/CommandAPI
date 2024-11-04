7c4d238b18a6c84d5715843bd77a7f3dcd90eac5
DerEchtePilz <81232921+DerEchtePilz@users.noreply.github.com>
2024-11-04 15:32:23 +0100

    Test toolkit doesn't really work right now

--- commandapi-platforms/commandapi-bukkit/commandapi-bukkit-test-toolkit/src/main/java/dev/jorel/commandapi/MockCommandAPIPlugin.java
+++ commandapi-platforms/commandapi-bukkit/commandapi-bukkit-test-toolkit/src/main/java/dev/jorel/commandapi/MockCommandAPIPlugin.java
@@ -15,7 +15,7 @@ import java.util.function.Consumer;
 public class MockCommandAPIPlugin extends JavaPlugin {
 	// Allow loading with settings,
 	//  Default to none if `MockBukkit.load(MockCommandAPIPlugin.class)` is used directly
-	private static Consumer<CommandAPIBukkitConfig> configureSettings = null;
+	private static Consumer<CommandAPIPaperConfig> configureSettings = null;
 
 	/**
 	 * Loads the CommandAPI plugin using {@link MockBukkit#load(Class)}.
@@ -29,18 +29,18 @@ public class MockCommandAPIPlugin extends JavaPlugin {
 	/**
 	 * Loads the CommandAPI plugin using {@link MockBukkit#load(Class)}.
 	 *
-	 * @param configureSettings A {@link Consumer} that can configure the settings of the {@link CommandAPIBukkitConfig}
+	 * @param configureSettings A {@link Consumer} that can configure the settings of the {@link dev.jorel.commandapi.CommandAPIPaperConfig}
 	 *                          before it is used to load the CommandAPI plugin.
 	 * @return The {@link MockCommandAPIPlugin} instance that was loaded.
 	 */
-	public static MockCommandAPIPlugin load(Consumer<CommandAPIBukkitConfig> configureSettings) {
+	public static MockCommandAPIPlugin load(Consumer<CommandAPIPaperConfig> configureSettings) {
 		MockCommandAPIPlugin.configureSettings = configureSettings;
 		return MockBukkit.load(MockCommandAPIPlugin.class);
 	}
 
 	@Override
 	public void onLoad() {
-		CommandAPIBukkitConfig config = new CommandAPIBukkitConfig(this);
+		CommandAPIPaperConfig config = new CommandAPIPaperConfig(this);
 
 		if (configureSettings != null) {
 			configureSettings.accept(config);
