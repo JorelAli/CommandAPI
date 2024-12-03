082bee75684e72a9300da1e6177277e104c2d1fd
DerEchtePilz <81232921+DerEchtePilz@users.noreply.github.com>
2024-12-03 20:25:57 +0100

    Initial work for 1.21.4 support
    And probably the final work too - except for the final stuff such as documentation and javadocs

--- commandapi-platforms/commandapi-bukkit/commandapi-bukkit-vh/src/main/java/dev/jorel/commandapi/CommandAPIVersionHandler.java
+++ commandapi-platforms/commandapi-bukkit/commandapi-bukkit-vh/src/main/java/dev/jorel/commandapi/CommandAPIVersionHandler.java
@@ -21,7 +21,22 @@
 package dev.jorel.commandapi;
 
 import dev.jorel.commandapi.exceptions.UnsupportedVersionException;
-import dev.jorel.commandapi.nms.*;
+import dev.jorel.commandapi.nms.NMS_1_16_R3;
+import dev.jorel.commandapi.nms.NMS_1_17;
+import dev.jorel.commandapi.nms.NMS_1_17_R1;
+import dev.jorel.commandapi.nms.NMS_1_18_R1;
+import dev.jorel.commandapi.nms.NMS_1_18_R2;
+import dev.jorel.commandapi.nms.NMS_1_19_1_R1;
+import dev.jorel.commandapi.nms.NMS_1_19_3_R2;
+import dev.jorel.commandapi.nms.NMS_1_19_4_R3;
+import dev.jorel.commandapi.nms.NMS_1_19_R1;
+import dev.jorel.commandapi.nms.NMS_1_20_R1;
+import dev.jorel.commandapi.nms.NMS_1_20_R2;
+import dev.jorel.commandapi.nms.NMS_1_20_R3;
+import dev.jorel.commandapi.nms.NMS_1_20_R4;
+import dev.jorel.commandapi.nms.NMS_1_21_R1;
+import dev.jorel.commandapi.nms.NMS_1_21_R2;
+import dev.jorel.commandapi.nms.NMS_1_21_R3;
 import org.bukkit.Bukkit;
 
 /**
@@ -51,7 +66,7 @@ public abstract class CommandAPIVersionHandler {
 	static LoadContext getPlatform() {
 		String latestMajorVersion = "21"; // Change this for Minecraft's major update
 		if (CommandAPI.getConfiguration().shouldUseLatestNMSVersion()) {
-			return new LoadContext(new NMS_1_21_R2(), () -> {
+			return new LoadContext(new NMS_1_21_R3(), () -> {
 				CommandAPI.logWarning("Loading the CommandAPI with the latest and potentially incompatible NMS implementation.");
 				CommandAPI.logWarning("While you may find success with this, further updates might be necessary to fully support the version you are using.");
 			});
@@ -73,6 +88,7 @@ public abstract class CommandAPIVersionHandler {
 				case "1.20.5", "1.20.6" -> new NMS_1_20_R4();
 				case "1.21", "1.21.1" -> new NMS_1_21_R1();
 				case "1.21.2", "1.21.3" -> new NMS_1_21_R2();
+				case "1.21.4" -> new NMS_1_21_R3();
 				default -> null;
 			};
 			if (platform != null) {
@@ -81,7 +97,7 @@ public abstract class CommandAPIVersionHandler {
 			if (CommandAPI.getConfiguration().shouldBeLenientForMinorVersions()) {
 				String currentMajorVersion = version.split("\\.")[1];
 				if (latestMajorVersion.equals(currentMajorVersion)) {
-					return new LoadContext(new NMS_1_21_R2(), () -> {
+					return new LoadContext(new NMS_1_21_R3(), () -> {
 						CommandAPI.logWarning("Loading the CommandAPI with a potentially incompatible NMS implementation.");
 						CommandAPI.logWarning("While you may find success with this, further updates might be necessary to fully support the version you are using.");
 					});
